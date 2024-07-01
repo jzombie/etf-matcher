604e36cfe32bbf22b917495384172caa67e46b88
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-01 14:13:23 -0600

    Experiment w/ multiple entity greeting

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,26 @@
 use wasm_bindgen::prelude::*;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
 }
 
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+}
+
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn greet(json_str: &str) {
+    match serde_json::from_str::<Vec<Person>>(json_str) {
+        Ok(persons) => {
+            let greetings: Vec<String> = persons.iter()
+                .map(|person| format!("Hello, {}!", person.name))
+                .collect();
+            alert(&greetings.join(" and "));
+        },
+        Err(_) => alert("Failed to parse JSON"),
+    }
 }
