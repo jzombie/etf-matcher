f47b235d3331cea907c621917015d41193a08017
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-03 15:23:11 -0600

    Fix issue where files in subdirectories were winding up in build main directory

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: "./**/*",
+          src: "./*",
           dest: DESTINATION_DIR,
         },
       ],
