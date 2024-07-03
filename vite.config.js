75803b2d253fb4b0783d05be031606730d3216b8
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-03 11:08:57 -0600

    Fix input.html input reference

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: "./index.html",
+      input: "./public/index.html",
       output: {
         dir: "dist",
         format: "es",
