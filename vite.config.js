1b796a9f46f19f062cd4403d3c2e9f1e3922bb00
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-03 15:16:55 -0600

    Add static assets to build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
+
+const DESTINATION_DIR = path.resolve(__dirname, "dist");
 
 export default defineConfig({
   root: "./public",
+  publicDir: false, // Disable the default publicDir handling
   build: {
     rollupOptions: {
       input: "./public/index.html",
       output: {
-        dir: "dist",
+        dir: DESTINATION_DIR, // Adjust the output directory to be outside of the public folder
         format: "es",
       },
     },
@@ -20,4 +25,14 @@ export default defineConfig({
       "Content-Encoding": "gzip",
     },
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./**/*",
+          dest: DESTINATION_DIR,
+        },
+      ],
+    }),
+  ],
 });
