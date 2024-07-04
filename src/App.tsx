0fc275b3e5f451fa1b880ca80df1b469d97a9bb3
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-04 15:06:41 -0600

    Clean up current prototype caller

--- src/App.tsx
+++ src/App.tsx
@@ -12,87 +12,25 @@ const App: React.FC = () => {
 export default App;
 
 (() => {
-  callWorkerFunction("count_etfs_per_exchange").then((data) => {
-    console.log({ data });
+  callWorkerFunction("get_symbols").then((symbols) => {
+    console.log({ symbols });
   });
 
-  // Call the function and log the result
-  callWorkerFunction("get_etf_holder_asset_count", "SPY")
+  callWorkerFunction("count_etfs_per_exchange")
+    .then((countsPerExchange) =>
+      console.log({
+        countsPerExchange,
+      })
+    )
+    .catch((error) => console.error(error));
+
+  const ETF_HOLDER_SYMBOL = "SPY";
+  callWorkerFunction("get_etf_holder_asset_count", ETF_HOLDER_SYMBOL)
     .then((assetCount) =>
       console.log({
+        etfHolder: ETF_HOLDER_SYMBOL,
         assetCount,
       })
     )
     .catch((error) => console.error(error));
-
-  callWorkerFunction("get_etf_holder_asset_count", "SPYG")
-    .then((assetCount) =>
-      console.log({
-        assetCount,
-      })
-    )
-    .catch((error) => console.error(error));
-
-  callWorkerFunction("get_etf_holder_asset_names", "SPYG")
-    .then((symbols) =>
-      console.log({
-        symbols,
-      })
-    )
-    .catch((error) => console.error(error));
-
-  callWorkerFunction("get_etf_holder_asset_names", "SPYG")
-    .then((symbols) =>
-      console.log({
-        symbols,
-      })
-    )
-    .catch((error) => console.error(error));
-  callWorkerFunction("get_etf_holder_asset_names", "SPYG")
-    .then((symbols) =>
-      console.log({
-        symbols,
-      })
-    )
-    .catch((error) => console.error(error));
-  callWorkerFunction("get_etf_holder_asset_names", "SPYG")
-    .then((symbols) =>
-      console.log({
-        symbols,
-      })
-    )
-    .catch((error) => console.error(error));
-  callWorkerFunction("get_etf_holder_asset_names", "SPYG")
-    .then((symbols) =>
-      console.log({
-        symbols,
-      })
-    )
-    .catch((error) => console.error(error));
-
-  setTimeout(() => {
-    callWorkerFunction("get_etf_holder_asset_names", "SPYG")
-      .then((symbols) =>
-        console.log({
-          symbols,
-        })
-      )
-      .catch((error) => console.error(error));
-  }, 5000);
-
-  callWorkerFunction("get_symbols", "SPYG")
-    .then((symbols) =>
-      console.log({
-        getSymbolsResult: symbols,
-      })
-    )
-    .catch((error) => console.error(error));
-
-  // callWorkerFunction("get_etf_holder", "TESTING123");
-  // .then((url) =>
-  //   console.log({
-  //     etfHolder: url,
-  //   })
-  // )
-  // .catch((error) => console.error(error));
 })();
