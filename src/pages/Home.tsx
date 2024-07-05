a0d25d612191915b25e49b6b7b7f3b09ea557b1f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-05 17:21:48 -0600

    Remove script tags

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,19 +18,8 @@ export default function Home() {
       <StockMarket colorTheme="dark" height={400} width="100%"></StockMarket>
       <TickerTape colorTheme="dark"></TickerTape>
       <Screener colorTheme="dark" width="100%" height={300}></Screener>
-
       <SymbolInfo colorTheme="dark" autosize></SymbolInfo>
-      {/* <div>
-        <a href="https://www.tradingview.com" rel="noopener" target="_blank">
-          <span className="blue-text">Stock Charts</span>
-        </a>{" "}
-        by TradingView
-      </div> */}
-      <script
-        type="text/javascript"
-        src="https://s3.tradingview.com/tv.js"
-      ></script>
-      <script type="text/javascript"></script>
+
       <PortfolioForm />
     </div>
   );
