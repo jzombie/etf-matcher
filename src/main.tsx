987a3c37c3f23ce4d91b5ded631b5d553011f8a5
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-05 15:07:14 -0600

    Initial `pages` structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About";
 
 // TODO: For GitHub Pages: Check URL; if 404.html, redirect to non-404 with same path and query
 const router = createBrowserRouter([
@@ -16,7 +12,7 @@ const router = createBrowserRouter([
   },
   {
     path: "about",
-    element: <div>About</div>,
+    element: <About />,
   },
 ]);
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,5 @@
+import "react";
+
+export default function About() {
+  return <div>About...</div>;
+}
