e3cb4fcf83b289b01ceca5e09786d4e385d17fad
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-06 13:03:24 -0600

    Fix issue where module aliases were not working in test

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,6 @@
 // vitest.config.js
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
@@ -7,4 +8,5 @@ export default defineConfig({
     environment: "jsdom",
     // setupFiles: "./test/setup.ts", // optional, if you need setup files
   },
+  plugins: [tsconfigPaths()],
 });
