c812ba91ef0e7acd2b774d026f5eda4e76fd55d0
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-06 19:52:08 -0600

    Add notes for implementing a transition wrapper

--- src/router.tsx
+++ src/router.tsx
@@ -20,3 +20,51 @@ export default createBrowserRouter([
     ],
   },
 ]);
+
+// Example flow if adding view transitions between routes:
+//
+// export default createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <TransitionWrapper />,
+//         children: [
+//           {
+//             path: "",
+//             element: <Home />,
+//           },
+//           {
+//             path: "about",
+//             element: <About />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ]);
+//
+// In the implementation, notice the usage of <Outlet /> for the children slotting.
+//
+// const TransitionWrapper = () => {
+//   const location = useLocation();
+
+//   return (
+//     <TransitionGroup>
+//       <CSSTransition
+//         key={location.key}
+//         timeout={300}
+//         classNames={{
+//           enter: styles['fade-enter'],
+//           enterActive: styles['fade-enter-active'],
+//           exit: styles['fade-exit'],
+//           exitActive: styles['fade-exit-active'],
+//         }}
+//       >
+//         <Outlet />
+//       </CSSTransition>
+//     </TransitionGroup>
+//   );
+// };
