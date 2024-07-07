977af006edabe38b3f9557b7baca0d4d9deafc6b
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-07 16:31:29 -0600

    Improve logic for visible symbols handling

--- src/components/SymbolContainer/SymbolContainerProvider.tsx
+++ src/components/SymbolContainer/SymbolContainerProvider.tsx
@@ -32,8 +32,16 @@ export default function SymbolContainerProvider({
   perSymbolThreshold = 0.5,
 }: SymbolContainerContextProps) {
   const observerRef = useRef<IntersectionObserver | null>(null);
+
+  // Contains mapping for registered observers
   const metadataMap = useRef(new Map<Element, string>());
-  const visibleSymbolsRef = useRef(new Set<string>());
+
+  // Contains mapping for currently visible observers
+  const visibleSymbolMap = useRef(new Map<Element, string>());
+
+  // const getVisibleSymbols = useCallback((): string[] => {
+  //   return [...new Set([...metadataMap.current.values()])];
+  // }, []);
 
   const handleObserve = useCallback((el: HTMLElement, tickerSymbol: string) => {
     if (observerRef.current) {
@@ -46,7 +54,10 @@ export default function SymbolContainerProvider({
     if (el && observerRef.current) {
       observerRef.current.unobserve(el);
       metadataMap.current.delete(el);
+      visibleSymbolMap.current.delete(el);
     }
+
+    console.log("visible symbols", [...visibleSymbolMap.current.values()]);
   }, []);
 
   useEffect(() => {
@@ -56,17 +67,17 @@ export default function SymbolContainerProvider({
         if (symbol) {
           if (entry.isIntersecting) {
             console.log(`Element with ticker symbol ${symbol} is visible.`);
-            visibleSymbolsRef.current.add(symbol);
+            visibleSymbolMap.current.set(entry.target, symbol);
           } else {
             console.log(`Element with ticker symbol ${symbol} is not visible.`);
-            visibleSymbolsRef.current.delete(symbol);
+            visibleSymbolMap.current.delete(entry.target);
           }
         } else {
           console.log("No symbol associated with the element.");
         }
       });
 
-      console.log("visible symbols", [...visibleSymbolsRef.current.values()]);
+      console.log("visible symbols", [...visibleSymbolMap.current.values()]);
     };
 
     observerRef.current = new IntersectionObserver(observerCallback, {
