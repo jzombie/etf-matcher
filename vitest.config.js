1d45229111376da52611b96bb55c4ac003dc5cf1
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-07 15:45:35 -0600

    Re-enable vitest ESLint config

--- vitest.config.js
+++ vitest.config.js
@@ -2,9 +2,7 @@
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 import checker from "vite-plugin-checker";
-// import eslint from "vite-plugin-eslint";
-
-console.warn("TODO: Enable eslint config in vitest.config.js");
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   test: {
@@ -12,10 +10,5 @@ export default defineConfig({
     environment: "jsdom",
     // setupFiles: "./test/setup.ts", // optional, if you need setup files
   },
-  plugins: [
-    checker({ typescript: true }),
-    tsconfigPaths(),
-
-    // eslint()
-  ],
+  plugins: [checker({ typescript: true }), tsconfigPaths(), eslint()],
 });
