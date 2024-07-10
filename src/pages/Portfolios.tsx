491d0736b24ae132b1581cfe2133c4659741e87a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-10 14:49:13 -0600

    Prototype portfolio ideas

--- src/pages/Portfolios.tsx
+++ src/pages/Portfolios.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Button } from "antd";
 
 export default function Portfolios() {
-  return <div>Portfolios</div>;
+  return (
+    <div>
+      <Button>Create new Portfolio</Button>
+      <hr />
+      <div>
+        <h2>My Portfolios</h2>
+        ...
+      </div>
+    </div>
+  );
 }
 
+// TODO: Make use of persistent session storage for portfolios, with ability to clear data
 // TODO: Enable import / export of Portfolios
 // TODO: Use Web Share (and Web Share Target [for PWAs]) API as potential transport agent
