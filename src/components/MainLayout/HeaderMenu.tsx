77dc0b7e0c63152432b24322453d81834263e727
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-11 13:37:13 -0600

    Initial extraction of `HeaderMenu` into separate component

---
+++ src/components/MainLayout/HeaderMenu.tsx
@@ -0,0 +1,47 @@
+import React, { useMemo } from "react";
+import { Menu } from "antd";
+import { HomeOutlined, SettingOutlined } from "@ant-design/icons";
+import { Link, matchPath, useLocation } from "react-router-dom";
+
+export default function HeaderMenu() {
+  const location = useLocation();
+
+  // Define the base paths for the main menu items
+  const menuItems = [
+    {
+      key: "/",
+      label: (
+        <Link to="/">
+          <HomeOutlined title="Home" />
+        </Link>
+      ),
+    },
+    // { key: "/sectors", label: <Link to="/sectors">Sectors</Link> },
+    { key: "/portfolios", label: <Link to="/portfolios">Portfolios</Link> },
+    { key: "/watchlists", label: <Link to="/watchlists">Watchlists</Link> },
+    {
+      key: "/settings",
+      label: (
+        <Link to="/settings">
+          <SettingOutlined title="Settings" />
+        </Link>
+      ),
+    },
+  ];
+
+  const selectedKey = menuItems.find(
+    (item) =>
+      matchPath({ path: item.key, end: true }, location.pathname) ||
+      matchPath({ path: `${item.key}/*`, end: false }, location.pathname)
+  )?.key;
+
+  return (
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      selectedKeys={selectedKey ? [selectedKey] : []}
+      items={menuItems}
+      style={{ flex: 1 }}
+    />
+  );
+}
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { TickerTape } from "react-ts-tradingview-widgets";
-import { Layout as AntLayout, Menu } from "antd";
-import { HomeOutlined, SettingOutlined } from "@ant-design/icons";
-import { Link, Outlet, useLocation, matchPath } from "react-router-dom";
+import { Layout as AntLayout } from "antd";
+import { Outlet } from "react-router-dom";
+
+import HeaderMenu from "./HeaderMenu";
+
 import useStoreStateReader from "@hooks/useStoreStateReader";
 import tradingViewCopyrightStyles from "@constants/tradingViewCopyrightStyles";
 import SearchModalButton from "../SearchModalButton";
@@ -12,37 +14,6 @@ const { Header, Content, Footer } = AntLayout;
 // TODO: Experiment w/ vintage newspaper design for background
 
 export default function Layout() {
-  const location = useLocation();
-
-  // Define the base paths for the main menu items
-  const menuItems = [
-    {
-      key: "/",
-      label: (
-        <Link to="/">
-          <HomeOutlined title="Home" />
-        </Link>
-      ),
-    },
-    // { key: "/sectors", label: <Link to="/sectors">Sectors</Link> },
-    { key: "/portfolios", label: <Link to="/portfolios">Portfolios</Link> },
-    { key: "/watchlists", label: <Link to="/watchlists">Watchlists</Link> },
-    {
-      key: "/settings",
-      label: (
-        <Link to="/settings">
-          <SettingOutlined title="Settings" />
-        </Link>
-      ),
-    },
-  ];
-
-  const selectedKey = menuItems.find(
-    (item) =>
-      matchPath({ path: item.key, end: true }, location.pathname) ||
-      matchPath({ path: `${item.key}/*`, end: false }, location.pathname)
-  )?.key;
-
   const {
     isProductionBuild,
     isRustInit,
@@ -68,13 +39,7 @@ export default function Layout() {
           justifyContent: "space-between",
         }}
       >
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          selectedKeys={selectedKey ? [selectedKey] : []}
-          items={menuItems}
-          style={{ flex: 1 }}
-        />
+        <HeaderMenu />
         {
           // TODO: Open search modal
         }
