160a349b37511323820680bd79bd447cae5fab6e
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-10 22:10:42 -0600

    Add TODO

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,6 +5,10 @@ export default function Settings() {
   return (
     <div>
       <Button onClick={() => alert("TODO: Implement")}>Clear Data</Button>
+
+      {
+        // TODO: Add configuration options to adjust tickers which show in the ticker tape in the footer
+      }
     </div>
   );
 }
