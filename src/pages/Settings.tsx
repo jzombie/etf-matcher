6b719d511ff33c6ae50240d715c83e2a08046d4a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-11 00:31:56 -0600

    Prototype notifications

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, notification, Space } from "antd";
 import useStoreStateReader from "@hooks/useStoreStateReader";
 
 export default function Settings() {
   const { symbolBuckets } = useStoreStateReader("symbolBuckets");
+
+  const [api, contextHolder] = notification.useNotification();
+
+  // TODO: Refactor into a system that can be called directly from the store
+  const openNotification = () => {
+    const key = `open${Date.now()}`;
+    const btn = (
+      <Space>
+        <Button type="link" size="small" onClick={() => api.destroy()}>
+          Destroy All
+        </Button>
+        <Button type="primary" size="small" onClick={() => api.destroy(key)}>
+          Confirm
+        </Button>
+      </Space>
+    );
+    api.open({
+      message: "Notification Title",
+      description:
+        'A function will be be called after the notification is closed (automatically after the "duration" time of manually).',
+      btn,
+      key,
+      // placement: "top",
+      // onClose: close,
+    });
+  };
 
   return (
     <div>
@@ -16,6 +42,17 @@ export default function Settings() {
           <div key={idx}>{symbolBucket.name}</div>
         ))}
       </div>
+
+      <hr />
+
+      <h2>Prototype Notifications</h2>
+
+      <>
+        {contextHolder}
+        <Button type="primary" onClick={openNotification}>
+          Open the notification box
+        </Button>
+      </>
 
       {
         // TODO: Add configuration options to adjust tickers which show in the ticker tape in the footer
