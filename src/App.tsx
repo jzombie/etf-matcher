ab682d5e76f4078757cf111c942157413ed798c9
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-11 14:00:20 -0600

    Update global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ConfigProvider, theme } from "antd";
-// import { Layout } from "antd";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { SymbolContainerProvider } from "@components/SymbolContainer";
@@ -9,11 +8,16 @@ export default function App() {
   return (
     <ConfigProvider
       theme={{
-        // 1. Use dark algorithm
+        token: {
+          // colorPrimary: "#1890ff", // Primary color
+          // colorBgBase: "#001529", // Base background color
+          // colorText: "#ffffff", // Text color for readability on dark backgrounds
+          // colorTextSecondary: "#8c8c8c", // Secondary text color
+          fontFamily: "'Roboto', sans-serif", // Custom font
+          // fontSize: 16, // Base font size
+          borderRadius: 16, // Border radius for a softer look
+        },
         algorithm: theme.darkAlgorithm,
-
-        // 2. Combine dark algorithm and compact algorithm
-        // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
       }}
     >
       <SymbolContainerProvider>
