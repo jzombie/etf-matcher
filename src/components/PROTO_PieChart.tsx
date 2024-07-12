2f5482bce3f7e1f620301b8ed97b4482d4220b73
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-12 06:54:51 -0600

    Prototype hardcoding of cache data for pie chart display

--- src/components/PROTO_PieChart.tsx
+++ src/components/PROTO_PieChart.tsx
@@ -1,53 +1,71 @@
 import React, { PureComponent } from "react";
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
 
-const data01 = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
+const cacheData = [
+  {
+    key: "/data/symbol_detail.0.enc",
+    size: 56688,
+    age: 99456,
+    last_accessed: 99456,
+    access_count: 1,
+  },
+  {
+    key: "/data/symbol_search_dict.enc",
+    size: 530948,
+    age: 105356,
+    last_accessed: 103277,
+    access_count: 3,
+  },
+  {
+    key: "/data/symbol_detail_index.enc",
+    size: 459,
+    age: 99556,
+    last_accessed: 99556,
+    access_count: 1,
+  },
+  {
+    key: "/data/symbol_etf_holders.18.enc",
+    size: 69977,
+    age: 101257,
+    last_accessed: 101257,
+    access_count: 1,
+  },
+  {
+    key: "/data/symbol_etf_holders_index.enc",
+    size: 3320,
+    age: 101348,
+    last_accessed: 101348,
+    access_count: 1,
+  },
 ];
-const data02 = [
-  { name: "A1", value: 100 },
-  { name: "A2", value: 300 },
-  { name: "B1", value: 100 },
-  { name: "B2", value: 80 },
-  { name: "B3", value: 40 },
-  { name: "B4", value: 30 },
-  { name: "B5", value: 50 },
-  { name: "C1", value: 100 },
-  { name: "C2", value: 200 },
-  { name: "D1", value: 150 },
-  { name: "D2", value: 50 },
-];
 
-export default class Example extends PureComponent {
-  // static demoUrl = 'https://codesandbox.io/s/pie-chart-of-two-levels-gor24';
+const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#00C49F"];
 
+export default class CachePieChart extends PureComponent {
   render() {
     return (
-      // <ResponsiveContainer width="100%" height="100%">
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data01}
-          dataKey="value"
-          cx="50%"
-          cy="50%"
-          outerRadius={60}
-          fill="#8884d8"
-        />
-        <Pie
-          data={data02}
-          dataKey="value"
-          cx="50%"
-          cy="50%"
-          innerRadius={70}
-          outerRadius={90}
-          fill="#82ca9d"
-          label
-        />
-      </PieChart>
-      // </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={400}>
+        <PieChart>
+          <Pie
+            data={cacheData}
+            dataKey="size"
+            nameKey="key"
+            cx="50%"
+            cy="50%"
+            outerRadius={150}
+            fill="#8884d8"
+            label
+          >
+            {cacheData.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
     );
   }
 }
