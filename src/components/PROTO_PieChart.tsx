1250bbd047c1f667a1b5a99e498627738f58a7ec
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-12 21:37:50 -0600

    Prototype `cacheDetails` rendering in pie chart

--- src/components/PROTO_PieChart.tsx
+++ src/components/PROTO_PieChart.tsx
@@ -1,71 +1,66 @@
 import React, { PureComponent } from "react";
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
 
-const cacheData = [
-  {
-    key: "/data/symbol_detail.0.enc",
-    size: 56688,
-    age: 99456,
-    last_accessed: 99456,
-    access_count: 1,
-  },
-  {
-    key: "/data/symbol_search_dict.enc",
-    size: 530948,
-    age: 105356,
-    last_accessed: 103277,
-    access_count: 3,
-  },
-  {
-    key: "/data/symbol_detail_index.enc",
-    size: 459,
-    age: 99556,
-    last_accessed: 99556,
-    access_count: 1,
-  },
-  {
-    key: "/data/symbol_etf_holders.18.enc",
-    size: 69977,
-    age: 101257,
-    last_accessed: 101257,
-    access_count: 1,
-  },
-  {
-    key: "/data/symbol_etf_holders_index.enc",
-    size: 3320,
-    age: 101348,
-    last_accessed: 101348,
-    access_count: 1,
-  },
-];
+import type { RustServiceCacheDetail } from "@src/store";
+import useStoreStateReader from "@hooks/useStoreStateReader";
 
+// TODO: Centralize somewhere else
 const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#00C49F"];
 
-export default class CachePieChart extends PureComponent {
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height={400}>
-        <PieChart>
-          <Pie
-            data={cacheData}
-            dataKey="size"
-            nameKey="key"
-            cx="50%"
-            cy="50%"
-            outerRadius={150}
-            fill="#8884d8"
-            label
-          >
-            {cacheData.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
-            ))}
-          </Pie>
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
-    );
-  }
+export default function CachePieChart() {
+  const { cacheDetails } = useStoreStateReader("cacheDetails");
+
+  return (
+    <ResponsiveContainer>
+      <PieChart>
+        <Pie
+          data={cacheDetails}
+          dataKey="size"
+          nameKey="key"
+          cx="50%"
+          cy="50%"
+          // outerRadius={80}
+          fill="#8884d8"
+          label
+        >
+          {cacheDetails.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        {
+          // TODO: Apply stying
+        }
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  );
 }
+
+// export default class CachePieChart extends PureComponent {
+//   render() {
+//     return (
+//       <ResponsiveContainer width="100%" height={400}>
+//         <PieChart>
+//           <Pie
+//             data={cacheData}
+//             dataKey="size"
+//             nameKey="key"
+//             cx="50%"
+//             cy="50%"
+//             outerRadius={150}
+//             fill="#8884d8"
+//             label
+//           >
+//             {cacheData.map((entry, index) => (
+//               <Cell
+//                 key={`cell-${index}`}
+//                 fill={COLORS[index % COLORS.length]}
+//               />
+//             ))}
+//           </Pie>
+//           <Tooltip />
+//         </PieChart>
+//       </ResponsiveContainer>
+//     );
+//   }
+// }
