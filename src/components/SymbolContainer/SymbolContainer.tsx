2a5b714e7ac5454d00f7854d1f3cfac8d81acb47
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-14 10:38:28 -0600

    Remove TODO that's non-specific to current file

--- src/components/SymbolContainer/SymbolContainer.tsx
+++ src/components/SymbolContainer/SymbolContainer.tsx
@@ -98,14 +98,6 @@ export default function SymbolContainer({
     handleFullRenderSymbolStateChange(shouldRenderSymbol);
   }, [shouldRenderSymbol, handleFullRenderSymbolStateChange]);
 
-  // TODO: Monitor time and percentage on screen and use to collect metrics
-  // about which symbols are looked at the longest. This isn't intended for
-  // invasive tracking, but is actually intended to help me personally understand
-  // which symbols I may be spending more time looking into without taking action.
-  //
-  // This state should tie into the store and persist locally to help build a
-  // profile.
-
   const elementRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
