6cbcd2a6ec5f7959d762f91aae3227d212c92cd0
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-14 15:10:38 -0600

    Initial render layout

--- src/components/SymbolDetailList.tsx
+++ src/components/SymbolDetailList.tsx
@@ -1,15 +1,8 @@
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import SymbolDetail from "./SymbolDetail";
 
 export type SymbolDetailListProps = {
   tickerSymbols: string[];
-  // onDeferredRenderStateChange?: (isDeferredRender: boolean) => void;
   lookAheadBufferSize?: number;
   lookAheadMaskStyle?: React.HTMLAttributes<HTMLDivElement>["style"];
 };
@@ -21,9 +14,7 @@ export default function SymbolDetailList({
     height: 500,
   },
 }: SymbolDetailListProps) {
-  // Add prop validation
   useEffect(() => {
-    // Validate groupTickerSymbols is unique
     if (tickerSymbols.length !== [...new Set(tickerSymbols)].length) {
       console.warn(
         "`groupTickerSymbols` is not unique! Unpredictable results may occur."
@@ -31,15 +22,12 @@ export default function SymbolDetailList({
     }
   }, [tickerSymbols]);
 
-  // Note: This should not be confused with `visibleSymbols` in the store state.
-  // `intersectingSymbols` refers to this component only, and `visibleSymbols` refers
-  // to the entire app.
   const [intersectingSymbols, setIntersectingSymbols] = useState<string[]>([]);
+  const [renderingIndex, setRenderingIndex] = useState<number>(0);
+  const maxIdxLastIntersecting = useRef<number>(-1);
 
   const handleIntersectionStateChange = useCallback(
     (tickerSymbol: string, isIntersecting: boolean) => {
-      console.log({ tickerSymbol, isIntersecting });
-
       setIntersectingSymbols((prevSymbols) => {
         const updatedSymbols = [...prevSymbols];
         const index = updatedSymbols.indexOf(tickerSymbol);
@@ -52,21 +40,20 @@ export default function SymbolDetailList({
             updatedSymbols.splice(index, 1);
           }
         }
-        // Sort the updatedSymbols array based on the order of tickerSymbols
         return updatedSymbols.sort(
           (a, b) => tickerSymbols.indexOf(a) - tickerSymbols.indexOf(b)
         );
       });
+
+      if (isIntersecting) {
+        setRenderingIndex((prevIndex) => prevIndex + 1);
+      }
     },
     [tickerSymbols]
   );
 
-  const maxIdxLastIntersecting = useRef<number>(-1);
-
   useEffect(() => {
     const lastIntersectingSymbol = intersectingSymbols.at(-1);
-
-    // TODO: Debug; this should only happen at initial mount stage
     if (!lastIntersectingSymbol) {
       return;
     }
@@ -77,11 +64,6 @@ export default function SymbolDetailList({
 
     if (idxGroupLastIntersecting > maxIdxLastIntersecting.current) {
       maxIdxLastIntersecting.current = idxGroupLastIntersecting;
-
-      // console.debug({
-      //   maxIdxPrevVisibleSymbol: maxIdxPrevVisibleSymbolRef.current,
-      //   lack: groupTickerSymbols.length - maxIdxPrevVisibleSymbolRef.current,
-      // });
     }
 
     console.log({ max: maxIdxLastIntersecting.current });
@@ -90,7 +72,7 @@ export default function SymbolDetailList({
   return (
     <>
       {tickerSymbols.map((tickerSymbol, idx) => {
-        if (idx <= maxIdxLastIntersecting.current + lookAheadBufferSize) {
+        if (idx <= renderingIndex + lookAheadBufferSize) {
           return (
             <SymbolDetail
               key={tickerSymbol}
@@ -106,7 +88,6 @@ export default function SymbolDetailList({
               key={tickerSymbol}
               style={{
                 ...lookAheadMaskStyle,
-                // backgroundColor: "yellow",
               }}
             />
           );
