3abbff3cda5d14dba7a92c6bc2973b9489fd19bd
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-15 22:59:48 -0600

    Prototype initial inclusion of stock logo

--- src/components/SymbolDetail.tsx
+++ src/components/SymbolDetail.tsx
@@ -2,26 +2,47 @@ import React, { useEffect, useState } from "react";
 import SymbolContainer from "./SymbolContainer";
 import { Button, Typography, Grid, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
 import Padding from "@layoutKit/Padding";
-
 import useStoreStateReader, { store } from "@hooks/useStoreStateReader";
 import type {
   RustServiceSymbolDetail,
   RustServiceETFHoldersWithTotalCount,
 } from "@utils/callWorkerFunction";
-
-import {
-  MiniChart,
-  // Timeline,
-  // CompanyProfile,
-} from "react-ts-tradingview-widgets";
+import { MiniChart } from "react-ts-tradingview-widgets";
 import tradingViewCopyrightStyles from "@constants/tradingViewCopyrightStyles";
+import ProtoStockLogoImg from "@assets/PROTO_stock_logo.png";
+import { styled } from "@mui/system";
 
 export type SymbolDetailProps = React.HTMLAttributes<HTMLDivElement> & {
   tickerSymbol: string;
   onIntersectionStateChange?: (isIntersecting: boolean) => void;
 };
+
+const LogoContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "0 50px 50px 0",
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(4),
+  marginRight: theme.spacing(2),
+}));
+
+const InfoContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-around",
+  width: "100%",
+}));
+
+const SymbolDetailWrapper = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  backgroundColor: theme.palette.background.default,
+  borderRadius: theme.shape.borderRadius,
+  // boxShadow: theme.shadows[1],
+  marginBottom: theme.spacing(4),
+}));
 
 export default function SymbolDetail({
   tickerSymbol,
@@ -29,18 +50,14 @@ export default function SymbolDetail({
   ...rest
 }: SymbolDetailProps) {
   const navigate = useNavigate();
-
   const { symbolBuckets } = useStoreStateReader(["symbolBuckets"]);
-
   const [symbolDetail, setSymbolDetail] = useState<
     RustServiceSymbolDetail | undefined
   >(undefined);
-
   const [etfHolders, setEtfHolders] = useState<
     RustServiceETFHoldersWithTotalCount | undefined
   >(undefined);
 
-  // Only query symbols that are fully rendered, or are next in the list
   useEffect(() => {
     if (tickerSymbol) {
       store.fetchSymbolDetail(tickerSymbol).then(setSymbolDetail);
@@ -55,9 +72,12 @@ export default function SymbolDetail({
       onIntersectionStateChange={onIntersectionStateChange}
       {...rest}
     >
-      <>
-        <Padding>
-          <Box mb={2}>
+      <SymbolDetailWrapper>
+        <LogoContainer>
+          <img src={ProtoStockLogoImg} alt="Logo" />
+        </LogoContainer>
+        <InfoContainer>
+          <Padding>
             <Grid container spacing={2}>
               <Grid item xs={12} md={6}>
                 <Typography variant="h6" component="div">
@@ -100,80 +120,43 @@ export default function SymbolDetail({
                 </Typography>
               </Grid>
             </Grid>
-          </Box>
-        </Padding>
-        <div style={{ height: 200 }}>
-          {
-            // https://tradingview-widgets.jorrinkievit.xyz/docs/components/MiniChart
-          }
-          <MiniChart
-            symbol={tickerSymbol}
-            colorTheme="dark"
-            width="100%"
-            height="100%"
-            copyrightStyles={tradingViewCopyrightStyles}
-            dateRange="ALL" // 1D / 1M / 3M / 12M / 60M / ALL
-            // TODO: Provide ability to configure date range
-            // isTransparent={true}
-          />
-        </div>
+          </Padding>
+        </InfoContainer>
+      </SymbolDetailWrapper>
 
-        {
-          // TODO: For the following, wrap with a div to prevent potential reflow issues
-        }
-
-        {/* <MiniChart
-            symbol={tickerSymbol}
-            colorTheme="dark"
-            width="100%"
-            copyrightStyles={tradingViewCopyrightStyles}
-          />
-          // TODO: Enable via toggle
-          /* <CompanyProfile
-            symbol={tickerSymbol}
-            width="100%"
-            height={300}
-            copyrightStyles={tradingViewCopyrightStyles}
-          /> */}
-        {/* <Timeline
-          colorTheme="dark"
-          feedMode="symbol"
+      <div style={{ height: 200 }}>
+        <MiniChart
           symbol={tickerSymbol}
-          height={400}
+          colorTheme="dark"
           width="100%"
+          height="100%"
           copyrightStyles={tradingViewCopyrightStyles}
-        /> */}
-        {symbolBuckets
-          ?.filter((symbolBucket) => symbolBucket.isUserConfigurable)
-          .map((symbolBucket, idx) => (
-            // TODO: If symbol is already in the bucket, don't try to re-add it
-            <Button
-              key={idx}
-              onClick={() =>
-                store.addSymbolToBucket(tickerSymbol, symbolBucket)
-              }
-            >
-              Add {tickerSymbol} to {symbolBucket.name}
-            </Button>
-          ))}
-        <div>
-          {
-            // TODO: Paginate through these results
-          }
-          {etfHolders?.results?.map((etfHolderSymbol) => (
-            <Button
-              key={etfHolderSymbol}
-              onClick={() =>
-                // TODO: Don't hardcode here
-                // TODO: Perhaps don't even navigate... just add the symbol to this view?
-                navigate(`/search?query=${etfHolderSymbol}&exact=true`)
-              }
-            >
-              {etfHolderSymbol}
-            </Button>
-          ))}
-        </div>
-      </>
+          dateRange="ALL"
+        />
+      </div>
+
+      {symbolBuckets
+        ?.filter((symbolBucket) => symbolBucket.isUserConfigurable)
+        .map((symbolBucket, idx) => (
+          <Button
+            key={idx}
+            onClick={() => store.addSymbolToBucket(tickerSymbol, symbolBucket)}
+          >
+            Add {tickerSymbol} to {symbolBucket.name}
+          </Button>
+        ))}
+      <div>
+        {etfHolders?.results?.map((etfHolderSymbol) => (
+          <Button
+            key={etfHolderSymbol}
+            onClick={() =>
+              navigate(`/search?query=${etfHolderSymbol}&exact=true`)
+            }
+          >
+            {etfHolderSymbol}
+          </Button>
+        ))}
+      </div>
     </SymbolContainer>
   );
 }
