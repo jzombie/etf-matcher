aa71ba4d5a1f92d1f3f0fffe60a1e8cd8b25ef29
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-21 17:07:53 -0600

    Configure `viteStaticCopy` to only run in production

--- vite.config.ts
+++ vite.config.ts
@@ -27,107 +27,115 @@ export function writeBuildTime() {
   return buildTime;
 }
 
-export default defineConfig({
-  root: "./public",
-  publicDir: false, // Disable the default publicDir handling
-  build: {
-    rollupOptions: {
-      input: "./public/index.html",
-      output: {
-        dir: DESTINATION_DIR, // Adjust the output directory to be outside of the public folder
-        format: "es",
-      },
-    },
-    emptyOutDir: true, // Ensure the output directory is emptied before each build
-  },
-  worker: {
-    format: "es",
-  },
-  preview: {
-    port: 8000,
-    headers: {
-      "Content-Encoding": "gzip",
-    },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 8000,
-    watch: {
-      // Ignoring this because of the large amount of files present in here, leading
-      // to stack overflow issues at times when rebuilding the data.
-      // (`viteStaticCopy` may need to be configured to work around this as well)
-      ignored: ["public/data"],
-    },
-  },
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "favicon.ico",
-          dest: path.resolve(DESTINATION_DIR),
-        },
-        {
-          src: "CNAME",
-          dest: path.resolve(DESTINATION_DIR),
-        },
-        {
-          src: "buildTime.json",
-          dest: path.resolve(DESTINATION_DIR),
-        },
-        {
-          src: "./data/*",
-          dest: path.resolve(DESTINATION_DIR, "data"),
-        },
-        {
-          src: "./pkg/*",
-          dest: path.resolve(DESTINATION_DIR, "pkg"),
-        },
-        {
-          src: "README.md",
-          dest: path.resolve(DESTINATION_DIR),
-        },
-      ],
-    }),
-    checker({ typescript: true }),
-    tsconfigPaths(),
-    eslint({
-      failOnError: false, // Show errors in console but do not fail build
-      failOnWarning: false, // Show warnings in console but do not fail build
-      cache: false, // Ensure ESLint re-evaluates on every save
-      fix: true, // Automatically fix linting errors where possible
-    }),
-    svgr(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          buildTime: writeBuildTime(), // Write build time to a file and inject it into HTML
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    root: "./public",
+    publicDir: false, // Disable the default publicDir handling
+    build: {
+      rollupOptions: {
+        input: "./public/index.html",
+        output: {
+          dir: DESTINATION_DIR, // Adjust the output directory to be outside of the public folder
+          format: "es",
         },
       },
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
+      emptyOutDir: true, // Ensure the output directory is emptied before each build
+    },
+    worker: {
+      format: "es",
+    },
+    preview: {
+      port: 8000,
+      headers: {
+        "Content-Encoding": "gzip",
       },
-    }),
-    sitemap({
-      hostname: "https://etfmatcher.com",
-      // TODO: Ideally these would seed automatically from `router.ts`
-      dynamicRoutes: [
-        "/",
-        "/search",
-        "/portfolios",
-        "/watchlists",
-        "/settings",
-      ],
-    }),
-  ],
-  // Resolve warnings with checker plugin (even though this is not a Vue project)
-  define: {
-    __VUE_OPTIONS_API__: JSON.stringify(true),
-    __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
-  },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 8000,
+      watch: {
+        // Ignoring this because of the large amount of files present in here, leading
+        // to stack overflow issues at times when rebuilding the data.
+        // (`viteStaticCopy` may need to be configured to work around this as well)
+        ignored: ["public/data"],
+      },
+    },
+    plugins: [
+      react(),
+      checker({ typescript: true }),
+      tsconfigPaths(),
+      eslint({
+        failOnError: false, // Show errors in console but do not fail build
+        failOnWarning: false, // Show warnings in console but do not fail build
+        cache: false, // Ensure ESLint re-evaluates on every save
+        fix: true, // Automatically fix linting errors where possible
+      }),
+      svgr(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            buildTime: writeBuildTime(), // Write build time to a file and inject it into HTML
+          },
+        },
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+      }),
+      sitemap({
+        hostname: "https://etfmatcher.com",
+        // TODO: Ideally these would seed automatically from `router.ts`
+        dynamicRoutes: [
+          "/",
+          "/search",
+          "/portfolios",
+          "/watchlists",
+          "/settings",
+        ],
+      }),
+      ...(isProduction
+        ? [
+            viteStaticCopy({
+              targets: [
+                {
+                  src: "favicon.ico",
+                  dest: path.resolve(DESTINATION_DIR),
+                },
+                {
+                  src: "CNAME",
+                  dest: path.resolve(DESTINATION_DIR),
+                },
+                {
+                  src: "buildTime.json",
+                  dest: path.resolve(DESTINATION_DIR),
+                },
+                {
+                  src: "./data/*",
+                  dest: path.resolve(DESTINATION_DIR, "data"),
+                },
+                {
+                  src: "./pkg/*",
+                  dest: path.resolve(DESTINATION_DIR, "pkg"),
+                },
+                {
+                  src: "README.md",
+                  dest: path.resolve(DESTINATION_DIR),
+                },
+              ],
+            }),
+          ]
+        : []),
+    ],
+    // Resolve warnings with checker plugin (even though this is not a Vue project)
+    define: {
+      __VUE_OPTIONS_API__: JSON.stringify(true),
+      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+    },
+  };
 });
