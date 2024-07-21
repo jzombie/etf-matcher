866bd7b7f7db561a6b76e220d62d9e3bd9c72977
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-21 11:32:05 -0600

    Add TODO and comments

--- src/hooks/useNavigationDirection.ts
+++ src/hooks/useNavigationDirection.ts
@@ -15,22 +15,30 @@ const useNavigationDirection = (): NavigationDirection => {
   const locationStack = useRef<{ key: string; pathname: string }[]>([]);
   const previousKey = useRef<string | null>(null);
 
+  // TODO: This still needs some slight improvement when handling forward
+  // arrow navigation, as it sometimes thinks it's a `backward` navigation
   useEffect(() => {
+    // Extract current path and key from the location object
     const currentPath = location.pathname;
     const currentKey = location.key;
 
     if (navigationType === "PUSH") {
+      // If the navigation type is "PUSH", add the current location to the stack
       locationStack.current.push({ key: currentKey, pathname: currentPath });
       setDirection(NavigationDirection.FORWARD);
     } else if (navigationType === "POP") {
+      // If the navigation type is "POP", check the last location in the stack
       const lastLocation = locationStack.current.pop();
       if (lastLocation && lastLocation.key !== currentKey) {
+        // If the last location key is different from the current key, set direction to "BACKWARD"
         setDirection(NavigationDirection.BACKWARD);
       } else {
+        // Otherwise, set direction to "FORWARD"
         setDirection(NavigationDirection.FORWARD);
       }
     }
 
+    // Update the previous key to the current key
     previousKey.current = currentKey;
   }, [location, navigationType]);
 
