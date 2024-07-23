a3f40c978212298ec15b2cdfd305a2f6b3143bf1
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-23 14:23:00 -0600

    Update TODO

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -1,4 +1,4 @@
-pub type TickerId = i32; // TODO: Convert to u64
+pub type TickerId = i32; // TODO: Convert to u32
 pub type ExchangeId = i32; // TODO: Convert to u32 (see TODO in `ticker_by_id` to not use -1 for corresponding type)
 pub type IndustryId = i32; // TODO: Convert to u32
 pub type SectorId = i32; // TODO: Convert to u32
