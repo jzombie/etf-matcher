a738a9802a77c08ad2734108774a78409e05af41
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-23 20:27:02 -0600

    `xhr_utils`

--- rust/src/utils/mod.rs
+++ rust/src/utils/mod.rs
@@ -6,14 +6,14 @@ pub mod fetch_and_decompress;
 pub mod parse;
 pub mod shard; // TODO: Remove
 pub mod shard_ng;
-pub mod xhr_fetch;
 pub mod ticker_utils;
 
 pub mod logo_utils;
 pub use logo_utils::extract_logo_filename;
 
+pub mod xhr_utils;
+pub use xhr_utils::xhr_fetch;
+
 // Re-export cache methods to be accessible from other modules
 pub use cache::{get_cache_future, insert_cache_future, remove_cache_entry};
 
-// Re-export to avoid doubled-up "extract_logo_filename::extract_logo_filename" in consumers
-pub use xhr_fetch::xhr_fetch;
