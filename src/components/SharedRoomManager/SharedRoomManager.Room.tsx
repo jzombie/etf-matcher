43cf9148d806f0cf3ad4e3d4fdb77772297d4994
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-03 01:43:36 -0600

    Improve layout

--- src/components/SharedRoomManager/SharedRoomManager.Room.tsx
+++ src/components/SharedRoomManager/SharedRoomManager.Room.tsx
@@ -1,6 +1,14 @@
 import React, { useCallback, useState } from "react";
 
-import { Button } from "@mui/material";
+import { Devices, ExitToApp, QrCode, Sync } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 import AutoScaler from "@layoutKit/AutoScaler";
 import store from "@src/store";
@@ -31,20 +39,59 @@ export default function Room({ room }: RoomProps) {
     store.PROTO_generateQRCode(roomShareURL).then(setQRCode);
   }, [getRoomShareURL, room]);
 
+  const toggleQRCode = useCallback(() => {
+    if (qrCode) {
+      setQRCode(null);
+    } else {
+      generateQRCode();
+    }
+  }, [qrCode, generateQRCode]);
+
   return (
-    <li key={room.roomName}>
-      {room.roomName}{" "}
-      <Button onClick={() => disconnectFromRoom(room)} color="secondary">
-        Disconnect
-      </Button>
-      <Button onClick={generateQRCode}>QR Code</Button>
-      {qrCode && (
-        <AutoScaler style={{ width: 150, height: 150 }}>
-          <div dangerouslySetInnerHTML={{ __html: qrCode }} />
-        </AutoScaler>
-      )}
-      <div>Currently connected devices: {room.peers.length + 1}</div>
-      <div>In sync: {room.isInSync ? "yes" : "no"}</div>
-    </li>
+    <Card variant="outlined" sx={{ mb: 2 }}>
+      <CardContent>
+        <Typography variant="h6" gutterBottom>
+          {room.roomName}
+        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1, mb: 2 }}>
+          <Button
+            onClick={() => disconnectFromRoom(room)}
+            color="secondary"
+            variant="outlined"
+            startIcon={<ExitToApp />}
+          >
+            Disconnect
+          </Button>
+          <Button
+            onClick={toggleQRCode}
+            variant="outlined"
+            startIcon={<QrCode />}
+          >
+            {!qrCode ? "Generate" : "Hide"} QR Code
+          </Button>
+        </Box>
+        {qrCode && (
+          <AutoScaler style={{ width: 150, height: 150, marginBottom: "1rem" }}>
+            <div dangerouslySetInnerHTML={{ __html: qrCode }} />
+          </AutoScaler>
+        )}
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Devices sx={{ mr: 1 }} />
+              <Typography>
+                Currently connected devices: {room.peers.length + 1}
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Sync sx={{ mr: 1 }} />
+              <Typography>In sync: {room.isInSync ? "yes" : "no"}</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
