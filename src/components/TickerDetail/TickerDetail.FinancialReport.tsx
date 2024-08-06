5195246e9f962e56918402276f9acea30d6696fd
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-06 14:32:51 -0600

    Use years as labels

--- src/components/TickerDetail/TickerDetail.FinancialReport.tsx
+++ src/components/TickerDetail/TickerDetail.FinancialReport.tsx
@@ -45,35 +45,45 @@ export default function FinancialReport({
 
   const data = [
     {
-      year: "4 Years Ago",
+      // year: "4 Years Ago",
+      year: financialData.calendar_year_4_yr,
+      //
       revenue: financialData.revenue_4_yr || 0,
       netIncome: financialData.net_income_4_yr || 0,
       operatingIncome: financialData.operating_income_4_yr || 0,
       operatingCashFlow: financialData.operating_cash_flow_4_yr || 0,
     },
     {
-      year: "3 Years Ago",
+      // year: "3 Years Ago",
+      year: financialData.calendar_year_3_yr,
+      //
       revenue: financialData.revenue_3_yr || 0,
       netIncome: financialData.net_income_3_yr || 0,
       operatingIncome: financialData.operating_income_3_yr || 0,
       operatingCashFlow: financialData.operating_cash_flow_3_yr || 0,
     },
     {
-      year: "2 Years Ago",
+      // year: "2 Years Ago",
+      year: financialData.calendar_year_2_yr,
+      //
       revenue: financialData.revenue_2_yr || 0,
       netIncome: financialData.net_income_2_yr || 0,
       operatingIncome: financialData.operating_income_2_yr || 0,
       operatingCashFlow: financialData.operating_cash_flow_2_yr || 0,
     },
     {
-      year: "1 Year Ago",
+      // year: "1 Year Ago",
+      year: financialData.calendar_year_1_yr,
+      //
       revenue: financialData.revenue_1_yr || 0,
       netIncome: financialData.net_income_1_yr || 0,
       operatingIncome: financialData.operating_income_1_yr || 0,
       operatingCashFlow: financialData.operating_cash_flow_1_yr || 0,
     },
     {
-      year: "Current",
+      // year: "Current",
+      year: financialData.calendar_year_current,
+      //
       revenue: financialData.revenue_current || 0,
       netIncome: financialData.net_income_current || 0,
       operatingIncome: financialData.operating_income_current || 0,
