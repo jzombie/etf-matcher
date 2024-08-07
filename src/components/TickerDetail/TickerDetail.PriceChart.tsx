ddda4bc7d496a72445b4788dcfa394719db2b0ae
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-06 19:26:02 -0600

    Make date range configurable and set default to 1 month

--- src/components/TickerDetail/TickerDetail.PriceChart.tsx
+++ src/components/TickerDetail/TickerDetail.PriceChart.tsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Box } from "@mui/material";
+import { Box, Button, ButtonGroup, Typography } from "@mui/material";
 
 import tradingViewCopyrightStyles from "@constants/tradingViewCopyrightStyles";
 import { MiniChart } from "react-ts-tradingview-widgets";
+import type { DateRange } from "react-ts-tradingview-widgets";
 
 export type PriceChartProps = {
   formattedSymbolWithExchange: string;
 };
 
+const dateRanges: DateRange[] = ["1D", "1M", "3M", "12M", "60M", "ALL"];
+
 export default function PriceChart({
   formattedSymbolWithExchange,
 }: PriceChartProps) {
+  const [dateRange, setDateRange] = useState<DateRange>("1M");
+
+  const handleDateRangeChange = (range: string) => {
+    setDateRange(range as DateRange);
+  };
+
   return (
-    <Box sx={{ height: 200 }}>
-      <MiniChart
-        symbol={formattedSymbolWithExchange}
-        colorTheme="dark"
-        width="100%"
-        height="100%"
-        copyrightStyles={tradingViewCopyrightStyles}
-        // TODO: Add date-range selection
-        dateRange="ALL"
-      />
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h6">Price Chart</Typography>
+        <ButtonGroup variant="outlined" aria-label="outlined button group">
+          {dateRanges.map((range) => (
+            <Button
+              key={range}
+              onClick={() => handleDateRangeChange(range)}
+              variant={dateRange === range ? "contained" : "outlined"}
+            >
+              {range}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </Box>
+      <Box sx={{ height: 200 }}>
+        <MiniChart
+          symbol={formattedSymbolWithExchange}
+          colorTheme="dark"
+          width="100%"
+          height="100%"
+          copyrightStyles={tradingViewCopyrightStyles}
+          dateRange={dateRange}
+        />
+      </Box>
     </Box>
   );
 }
