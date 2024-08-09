9823740b27f3e495625bfd074641f4c12d293bdf
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-08 18:30:19 -0600

    Extract `TickerBucketList`

---
+++ src/components/SettingsManager/SettingsManager.UserDataSection/TickerBucketList.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import { Typography } from "@mui/material";
+
+import { TickerBucketProps } from "@src/store";
+
+import Section from "@components/Section";
+
+import TickerBucketItem from "./TickerBucketItem";
+
+export type TickerBucketListProps = {
+  tickerBuckets: TickerBucketProps[];
+};
+
+export default function TickerBucketList({
+  tickerBuckets,
+}: TickerBucketListProps) {
+  return (
+    <ul style={{ listStyle: "none", padding: 0 }}>
+      {tickerBuckets?.map((tickerBucket, idx) => (
+        <li key={idx}>
+          <Section>
+            <AssessmentIcon
+              sx={{
+                fontSize: 24,
+                display: "inline-block",
+                verticalAlign: "middle",
+                marginRight: 1,
+              }}
+            />
+            <Typography variant="body1" sx={{ display: "inline-block" }}>
+              <span style={{ fontWeight: "bold" }}>{tickerBucket.name}</span>:{" "}
+              <span
+                style={{
+                  fontStyle: "italic",
+                  fontSize: ".8rem",
+                }}
+              >
+                ({tickerBucket.tickers.length} item
+                {tickerBucket.tickers.length !== 1 ? "s" : ""})
+              </span>
+            </Typography>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "8px",
+                marginTop: "8px",
+              }}
+            >
+              {tickerBucket.tickers.map((ticker) => (
+                <TickerBucketItem
+                  key={ticker.tickerId}
+                  tickerBucketTicker={ticker}
+                />
+              ))}
+            </div>
+          </Section>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/components/SettingsManager/SettingsManager.UserDataSection/UserDataSection.tsx
+++ src/components/SettingsManager/SettingsManager.UserDataSection/UserDataSection.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState } from "react";
 
-import AssessmentIcon from "@mui/icons-material/Assessment";
 import {
   Button,
   Dialog,
@@ -8,14 +7,13 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  Typography,
 } from "@mui/material";
 
 import Section from "@components/Section";
 
 import useStoreStateReader, { store } from "@hooks/useStoreStateReader";
 
-import TickerBucketItem from "./TickerBucketItem";
+import TickerBucketList from "./TickerBucketList";
 
 // TODO: Also include any other user history (i.e. recently joined rooms, etc.)
 export default function UserDataSection() {
@@ -65,52 +63,7 @@ export default function UserDataSection() {
           Clear User Data
         </Button>
         <h3>Buckets</h3>
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          {tickerBuckets?.map((tickerBucket, idx) => (
-            <li key={idx}>
-              <Section>
-                <AssessmentIcon
-                  sx={{
-                    fontSize: 24,
-                    display: "inline-block",
-                    verticalAlign: "middle",
-                    marginRight: 1,
-                  }}
-                />
-                <Typography variant="body1" sx={{ display: "inline-block" }}>
-                  <span style={{ fontWeight: "bold" }}>
-                    {tickerBucket.name}
-                  </span>
-                  :{" "}
-                  <span
-                    style={{
-                      fontStyle: "italic",
-                      fontSize: ".8rem",
-                    }}
-                  >
-                    ({tickerBucket.tickers.length} item
-                    {tickerBucket.tickers.length !== 1 ? "s" : ""})
-                  </span>
-                </Typography>
-                <div
-                  style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    gap: "8px",
-                    marginTop: "8px",
-                  }}
-                >
-                  {tickerBucket.tickers.map((ticker) => (
-                    <TickerBucketItem
-                      key={ticker.tickerId}
-                      tickerBucketTicker={ticker}
-                    />
-                  ))}
-                </div>
-              </Section>
-            </li>
-          ))}
-        </ul>
+        <TickerBucketList tickerBuckets={tickerBuckets} />
       </Section>
 
       <Dialog
