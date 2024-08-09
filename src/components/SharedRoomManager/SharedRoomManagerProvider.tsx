ddbaa92c1d90f4877380ce38c82166f5e3945c10
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-08 18:54:17 -0600

    Move TODO into existing

--- src/components/SharedRoomManager/SharedRoomManagerProvider.tsx
+++ src/components/SharedRoomManager/SharedRoomManagerProvider.tsx
@@ -66,8 +66,10 @@ export default function SharedRoomManagerProvider({
 
   // Handle over-the-wire state sync
   //
-  // TODO: For initial received state updates, there should be a UI dialog confirmation
-  // to export the current state before overwriting the local state with the update
+  // TODO: On the initial message (assuming there is a retained message, etc.), determine
+  // if the store is in a `clean` initial state (either at `initial` state or at post-async
+  // init) and if not in the clean state, prompt the user for how they wish to handle the
+  // first incoming merge
   useEffect(() => {
     // Flag to determine if local state set should be immediately synced to peers
     let isOutboundSyncPaused = false;
@@ -88,10 +90,6 @@ export default function SharedRoomManagerProvider({
 
     store.on(StateEmitterDefaultEvents.UPDATE, _handleSendUpdate);
 
-    // TODO: On the initial message (assuming there is a retained message, etc.), determine
-    // if the store is in a `clean` initial state (either at `initial` state or at post-async
-    // init) and if not in the clean state, prompt the user for how they wish to handle the
-    // first incoming merge
     const _handleReceiveUpdate = (
       receivedData: MQTTRoomEvents["message"][0],
     ) => {
