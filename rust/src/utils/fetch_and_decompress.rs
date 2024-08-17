08a803d832bf3f253fe855241be9fc7dcea55e6c
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-16 19:43:18 -0600

    Initial migration to shared cache

--- rust/src/utils/fetch_and_decompress.rs
+++ rust/src/utils/fetch_and_decompress.rs
@@ -7,12 +7,13 @@ use hex;
 use std::convert::TryInto;
 use std::io::Read;
 use wasm_bindgen::prelude::*;
+use std::sync::Arc;
 
 use crate::utils::decrypt::password::{
     decrypt_password, get_encrypted_password, get_iv, Aes256Cbc,
 };
 use crate::utils::xhr_fetch;
-use crate::utils::{get_cache_future, insert_cache_future, remove_cache_entry};
+use crate::utils::{get_shared_cache_future, insert_shared_cache_future, remove_shared_cache_entry};
 
 pub async fn fetch_and_decompress_gz<T>(url: T, use_cache: bool) -> Result<Vec<u8>, JsValue>
 where
@@ -21,10 +22,10 @@ where
     let url_str: String = url.as_ref().to_string();
 
     if use_cache {
-        if let Some(shared_future) = get_cache_future(&url_str) {
+        if let Some(shared_future) = get_shared_cache_future(&url_str) {
             let result = shared_future.await;
-            return result.map_err(|err| {
-                remove_cache_entry(&url_str);
+            return result.map(|data| (*data).clone()).map_err(|err| {
+                remove_shared_cache_entry(&url_str);
                 JsValue::from_str(&format!("Error: {:?}", err))
             });
         }
@@ -32,20 +33,22 @@ where
         let future = decrypt_and_decompress_data(url_str.clone())
             .boxed_local()
             .shared();
-        insert_cache_future(&url_str, future.clone());
+        insert_shared_cache_future(&url_str, future.clone());
         let result = future.await;
 
-        result.map_err(|err| {
-            remove_cache_entry(&url_str);
+        result.map(|data| (*data).clone()).map_err(|err| {
+            remove_shared_cache_entry(&url_str);
             JsValue::from_str(&format!("Error: {:?}", err))
         })
     } else {
         web_sys::console::debug_1(&"Skipping cache".into());
-        decrypt_and_decompress_data(url_str).await
+        // Convert the Arc<Vec<u8>> to Vec<u8> here
+        decrypt_and_decompress_data(url_str).await.map(|data| (*data).clone())
     }
 }
 
-async fn decrypt_and_decompress_data(url: String) -> Result<Vec<u8>, JsValue> {
+
+async fn decrypt_and_decompress_data(url: String) -> Result<Arc<Vec<u8>>, JsValue> {
     let encrypted_data: Vec<u8> = xhr_fetch(url).await?;
 
     let salt: &[u8] = &encrypted_data[0..16];
@@ -75,5 +78,5 @@ async fn decrypt_and_decompress_data(url: String) -> Result<Vec<u8>, JsValue> {
         JsValue::from_str(&format!("Failed to decompress data: {}", err))
     })?;
 
-    Ok(decompressed_data)
+    Ok(Arc::new(decompressed_data))
 }
