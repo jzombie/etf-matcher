4c2cc9397e9402ba654577cc1f61f6d15b9ac0f5
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-17 22:55:52 -0600

    Prototype scatter plot types

--- src/components/TickerDetail/TickerDetail.PCAScatterPlot.tsx
+++ src/components/TickerDetail/TickerDetail.PCAScatterPlot.tsx
@@ -1,12 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-// TODO: Enable TS checking for this
 import React, { useCallback, useEffect, useState } from "react";
 
 import AutoScaler from "@layoutKit/AutoScaler";
 import store from "@src/store";
 import { RustServiceTickerDetail } from "@src/types";
-import { Scatter, ScatterChart, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Scatter,
+  ScatterChart,
+  Tooltip,
+  TooltipProps,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { NameType } from "recharts/types/component/DefaultTooltipContent";
 
 import customLogger from "@utils/customLogger";
 
@@ -16,10 +21,18 @@ export type PCAScatterPlotProps = {
   tickerDetail: RustServiceTickerDetail;
 };
 
+type ChartVectorDistance = {
+  ticker_id: RustServiceTickerDetail["ticker_id"];
+  pc1: number;
+  pc2: number;
+};
+
 // This component mimics a radial scatter plot, which `ReCharts` doesn't directly support.
 // The coordinates are based directly on the PCA coordinates.
 export default function PCAScatterPlot({ tickerDetail }: PCAScatterPlotProps) {
-  const [chartData, setChartData] = useState(null);
+  const [chartData, setChartData] = useState<ChartVectorDistance[] | null>(
+    null,
+  );
 
   useEffect(() => {
     if (tickerDetail) {
@@ -28,17 +41,17 @@ export default function PCAScatterPlot({ tickerDetail }: PCAScatterPlotProps) {
         .then((data) =>
           data.map((item) => ({
             ticker_id: item.ticker_id,
-            pc1: item.translated_pca_coordinates[0],
-            pc2: item.translated_pca_coordinates[1],
+            pc1: item.translated_pca_coords[0],
+            pc2: item.translated_pca_coords[1],
           })),
         )
         .then(setChartData);
     }
   }, [tickerDetail]);
 
-  const handleClick = useCallback((chartData) => {
+  const handleClick = useCallback((item: ChartVectorDistance) => {
     customLogger.debug(
-      `Ticker ID: ${chartData.ticker_id}\nPC1: ${chartData.pc1}\nPC2: ${chartData.pc2}`,
+      `Ticker ID: ${item.ticker_id}\nPC1: ${item.pc1}\nPC2: ${item.pc2}`,
     );
   }, []);
 
@@ -47,7 +60,11 @@ export default function PCAScatterPlot({ tickerDetail }: PCAScatterPlotProps) {
   }
 
   // Custom tooltip content
-  const CustomTooltip = ({ active, payload }) => {
+  // https://github.com/recharts/recharts/issues/2796
+  const CustomTooltip: React.FC<TooltipProps<number, NameType>> = ({
+    active,
+    payload,
+  }) => {
     if (active && payload && payload.length) {
       const { ticker_id, pc1, pc2 } = payload[0].payload;
       return (
@@ -62,13 +79,21 @@ export default function PCAScatterPlot({ tickerDetail }: PCAScatterPlotProps) {
   };
 
   // Custom scatter point content
-  const renderCustomPoint = (props) => {
-    const { cx, cy, payload } = props;
+  const renderCustomPoint = (props: {
+    cx?: number;
+    cy?: number;
+    payload?: ChartVectorDistance;
+  }): JSX.Element => {
+    // eslint-disable-next-line react/prop-types
+    const { cx = 0, cy = 0, payload } = props;
     return (
       <g>
         <circle cx={cx} cy={cy} r={6} fill="#8884d8" />
         <text x={cx} y={cy} dy={-10} textAnchor="middle" fill="#333">
-          {payload.ticker_id}
+          {
+            // eslint-disable-next-line react/prop-types
+            payload?.ticker_id
+          }
         </text>
       </g>
     );
