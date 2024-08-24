e5be3292e55e7e7a656d4b3a8fcc92dadc847d0d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-23 22:24:59 -0600

    Update `Session Syncing` verbiage (#89)

--- src/components/SettingsManager/SettingsManager.tsx
+++ src/components/SettingsManager/SettingsManager.tsx
@@ -63,17 +63,23 @@ export default function SettingsManager() {
     <Scrollable>
       <Padding>
         <Section>
-          <h2>Session Sharing</h2>
+          <h2>Session Syncing (Beta)</h2>
           <Typography variant="body2" component="p">
-            ETF Matcher doesn&apos;t use user accounts, but session data can be
-            retained and shared with other devices in real-time.
+            Use a unique session name to link your devices and share session
+            data in real-time.
           </Typography>
           <Typography variant="body2" component="p" mt={1}>
-            Your devices can be linked using a common room name.
+            You can create multiple sessions, each with its own unique name, to
+            keep your data organized across different devices.
           </Typography>
-          <Typography variant="body2" component="p" mt={1}>
-            Multiple rooms can also be used simultaneously for different
-            sessions.
+          <Typography
+            variant="body2"
+            component="p"
+            mt={1}
+            sx={{ fontStyle: "italic" }}
+          >
+            This feature is currently in beta and it can be unreliable, and
+            prone to disconnect without warning.
           </Typography>
 
           <Box mt={2}>
