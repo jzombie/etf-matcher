af56501a541ad22bc3c5b3bc88a2d9a7befd8121
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-08-28 13:05:47 -0600

    Add `BrowserRouter` to `customRender`

--- test/customRender.tsx
+++ test/customRender.tsx
@@ -2,13 +2,23 @@ import React from "react";
 
 import { RenderOptions, render } from "@testing-library/react";
 
+import { BrowserRouter } from "react-router-dom";
+
 import NotificationProvider from "../src/components/NotificationProvider";
+
+const AllProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <BrowserRouter>
+      <NotificationProvider>{children}</NotificationProvider>
+    </BrowserRouter>
+  );
+};
 
 const customRender = (
   ui: React.ReactElement,
   options?: Omit<RenderOptions, "wrapper">,
 ) => {
-  return render(ui, { wrapper: NotificationProvider, ...options });
+  return render(ui, { wrapper: AllProviders, ...options });
 };
 
 export * from "@testing-library/react";
