e7f6099ac70c7cb11da4958f79ee14bbbc4a4731
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-06 15:03:13 -0600

    Prototype `react-mosaic` integration

--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import AutoScaler from "@layoutKit/AutoScaler";
 import Layout, { Content, Footer, Header } from "@layoutKit/Layout";
+import { MosaicNode } from "react-mosaic-component";
 
 import HistoricalPriceChart from "@components/TickerDetail/TickerDetail.HistoricalPriceChart";
 import WindowManager from "@components/WindowManager";
@@ -25,11 +26,12 @@ const contentMap = {
   "Similarity Search": <div>Similarity Search content</div>,
 };
 
-const initialValue = {
-  direction: "column",
+// Use 'row' and 'column' instead of generic strings
+const initialValue: MosaicNode<string> = {
+  direction: "column", // Use 'column'
   first: "Detail",
   second: {
-    direction: "row",
+    direction: "row", // Use 'row'
     first: "Historical Prices",
     second: "Similarity Search",
   },
