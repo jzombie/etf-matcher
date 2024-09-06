4850a03c61148acb277a1032bb683a6f9ecf1cb2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-05 19:09:20 -0600

    Prototype data rendering (still fake data)

--- src/components/SectorsRadarChart.tsx
+++ src/components/SectorsRadarChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Legend,
@@ -13,84 +13,35 @@ import {
 import { getAllMajorSectors } from "@utils/callRustService";
 import customLogger from "@utils/customLogger";
 
-// TODO: Don't hardcode
-const data = [
-  {
-    subject: "Basic Materials",
-    A: 120,
-    B: 110,
-    fullMark: 150,
-  },
-  {
-    subject: "Consumer Discretionary",
-    A: 98,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: "Consumer Staples",
-    A: 86,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: "Energy",
-    A: 99,
-    B: 100,
-    fullMark: 150,
-  },
-  {
-    subject: "Financials",
-    A: 85,
-    B: 90,
-    fullMark: 150,
-  },
-  {
-    subject: "Healthcare",
-    A: 65,
-    B: 85,
-    fullMark: 150,
-  },
-  {
-    subject: "Industrials",
-    A: 35,
-    B: 25,
-    fullMark: 150,
-  },
-  {
-    subject: "Information Technology",
-    A: 10,
-    B: 90,
-    fullMark: 150,
-  },
-  {
-    subject: "Real Estate",
-    A: 45,
-    B: 95,
-    fullMark: 150,
-  },
-  {
-    subject: "Telecommunication Services",
-    A: 42,
-    B: 93,
-    fullMark: 150,
-  },
-  {
-    subject: "Utilities",
-    A: 45,
-    B: 150,
-    fullMark: 150,
-  },
-];
+type SectorData = {
+  subject: string;
+  A: number;
+  B: number;
+  fullMark: number;
+};
 
 export default function SectorsRadarChart() {
-  // TODO: Build out
+  const [data, setData] = useState<SectorData[]>([]);
+
   useEffect(() => {
-    getAllMajorSectors().then(customLogger.debug);
+    getAllMajorSectors().then((sectorMap) => {
+      // TODO: Implement actual data
+
+      customLogger.debug(sectorMap);
+
+      // Dynamically generate the data
+      const generatedData = Array.from(sectorMap).map(([_, sectorName]) => ({
+        subject: sectorName,
+        A: Math.floor(Math.random() * 150), // Random value for A (TODO: Update logic)
+        B: Math.floor(Math.random() * 150), // Random value for B (TODO: Update logic)
+        fullMark: 150,
+      }));
+
+      setData(generatedData);
+    });
   }, []);
 
   return (
-    // TODO: Don't hardcode
     <ResponsiveContainer width="100%" height={200}>
       <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
         <PolarGrid />
@@ -103,13 +54,13 @@ export default function SectorsRadarChart() {
           fill="#8884d8"
           fillOpacity={0.6}
         />
-        {/* <Radar
+        <Radar
           name="B"
           dataKey="B"
           stroke="#82ca9d"
           fill="#82ca9d"
           fillOpacity={0.6}
-        /> */}
+        />
         <Legend />
       </RadarChart>
     </ResponsiveContainer>
