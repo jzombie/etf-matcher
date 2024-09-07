dd0d0915deb7559ef5365a588a3561637a4db6e9
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-07 10:53:13 -0600

    Add `useTickerViewWindowManagerContent`

--- src/components/TickerViewWindowManager/TickerViewWindowManager.tsx
+++ src/components/TickerViewWindowManager/TickerViewWindowManager.tsx
@@ -1,32 +1,17 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { Box, Button } from "@mui/material";
 
-import AutoScaler from "@layoutKit/AutoScaler";
-import Center from "@layoutKit/Center";
 import Layout, { Content, Footer, Header } from "@layoutKit/Layout";
-import Scrollable from "@layoutKit/Scrollable";
 import { MosaicNode, MosaicParent } from "react-mosaic-component";
 
-import SectorsPieChart from "@components/SectorsPieChart";
 import TickerContainer from "@components/TickerContainer";
 import WindowManager from "@components/WindowManager";
 
-import useTickerDetail from "@hooks/useTickerDetail";
-
-import {
-  RustServiceETFAggregateDetail,
-  fetchETFAggregateDetailByTickerId,
-} from "@utils/callRustService";
 import customLogger from "@utils/customLogger";
-import formatSymbolWithExchange from "@utils/string/formatSymbolWithExchange";
 
 import TickerViewWindowManagerBucketManager from "./TickerViewWindowManager.BucketManager";
-import ETFHolderList from "./applets/ETFHolderList.applet";
-import ETFHoldingList from "./applets/ETFHoldingList.applet";
-import HistoricalPriceChart from "./applets/HistoricalPriceChart.applet";
-import PCAScatterPlot from "./applets/PCAScatterPlot.applet";
-import TickerInformation from "./applets/TickerInformation.applet";
+import useTickerViewWindowManagerContent from "./useTickerViewWindowManagerContent";
 
 export type TickerViewWindowManagerProps = {
   tickerId: number;
@@ -35,19 +20,8 @@ export type TickerViewWindowManagerProps = {
 export default function TickerViewWindowManager({
   tickerId,
 }: TickerViewWindowManagerProps) {
-  const { tickerDetail, isLoadingTickerDetail } = useTickerDetail(tickerId);
-
-  // Refactor: Save ETF aggregate detail
-  const [etfAggregateDetail, setETFAggregateDetail] = useState<
-    RustServiceETFAggregateDetail | undefined
-  >(undefined);
-  useEffect(() => {
-    if (tickerDetail?.is_etf) {
-      fetchETFAggregateDetailByTickerId(tickerDetail.ticker_id).then(
-        setETFAggregateDetail,
-      );
-    }
-  }, [tickerDetail]);
+  const { initialValue, contentMap, tickerDetail } =
+    useTickerViewWindowManagerContent(tickerId);
 
   // Track layout, open windows, saved layouts, and split percentages
   const [layout, setLayout] = useState<MosaicNode<string> | null>(null);
@@ -113,99 +87,6 @@ export default function TickerViewWindowManager({
       }
     },
     [openWindows, savedLayouts, splitPercentages],
-  );
-
-  // Initial layout definition
-  const initialValue: MosaicNode<string> = useMemo(
-    () => ({
-      direction: "column",
-      first: {
-        direction: "row",
-        first: "Ticker Information",
-        second: {
-          direction: "row",
-          first: "Historical Prices",
-          second: {
-            direction: "row",
-            first: "Similarity Search",
-            second: "Sector Allocation",
-            splitPercentage: 50,
-          },
-          splitPercentage: 55.6,
-        },
-        splitPercentage: 25,
-      },
-      second: {
-        direction: "row",
-        first: "ETF Holders",
-        second: "ETF Holdings",
-        splitPercentage: 50,
-      },
-      splitPercentage: 30,
-    }),
-    [],
-  );
-
-  // Map content to window titles
-  const contentMap = useMemo(
-    () => ({
-      "Ticker Information":
-        isLoadingTickerDetail || !tickerDetail ? (
-          <Center>Loading</Center>
-        ) : (
-          <TickerInformation tickerDetail={tickerDetail} />
-        ),
-      "ETF Holders":
-        isLoadingTickerDetail || !tickerDetail ? (
-          <Center>Loading</Center>
-        ) : (
-          <Scrollable>
-            <ETFHolderList tickerDetail={tickerDetail} />
-          </Scrollable>
-        ),
-      "ETF Holdings":
-        isLoadingTickerDetail || !tickerDetail ? (
-          <Center>Loading</Center>
-        ) : (
-          <Scrollable>
-            <ETFHoldingList etfTickerDetail={tickerDetail} />
-          </Scrollable>
-        ),
-      "Historical Prices":
-        isLoadingTickerDetail || !tickerDetail ? (
-          <Center>Loading</Center>
-        ) : (
-          <AutoScaler>
-            <HistoricalPriceChart
-              tickerSymbol={tickerDetail?.symbol}
-              formattedSymbolWithExchange={formatSymbolWithExchange(
-                tickerDetail,
-              )}
-            />
-          </AutoScaler>
-        ),
-      "Similarity Search":
-        isLoadingTickerDetail || !tickerDetail ? (
-          <Center>Loading</Center>
-        ) : (
-          <AutoScaler>
-            <PCAScatterPlot tickerDetail={tickerDetail} />
-          </AutoScaler>
-        ),
-      "Sector Allocation":
-        isLoadingTickerDetail || !etfAggregateDetail ? (
-          <Center>Loading</Center>
-        ) : (
-          etfAggregateDetail?.major_sector_distribution && (
-            <SectorsPieChart
-              majorSectorDistribution={
-                etfAggregateDetail.major_sector_distribution
-              }
-            />
-          )
-        ),
-    }),
-    [etfAggregateDetail, isLoadingTickerDetail, tickerDetail],
   );
 
   useEffect(() => {
---
+++ src/components/TickerViewWindowManager/useTickerViewWindowManagerContent.tsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useMemo, useState } from "react";
+
+import AutoScaler from "@layoutKit/AutoScaler";
+import Center from "@layoutKit/Center";
+import Scrollable from "@layoutKit/Scrollable";
+import { MosaicNode } from "react-mosaic-component";
+
+import SectorsPieChart from "@components/SectorsPieChart";
+
+import useTickerDetail from "@hooks/useTickerDetail";
+
+import {
+  RustServiceETFAggregateDetail,
+  fetchETFAggregateDetailByTickerId,
+} from "@utils/callRustService";
+import formatSymbolWithExchange from "@utils/string/formatSymbolWithExchange";
+
+import ETFHolderList from "./applets/ETFHolderList.applet";
+import ETFHoldingList from "./applets/ETFHoldingList.applet";
+import HistoricalPriceChart from "./applets/HistoricalPriceChart.applet";
+import PCAScatterPlot from "./applets/PCAScatterPlot.applet";
+import TickerInformation from "./applets/TickerInformation.applet";
+
+export default function useTickerViewWindowManagerContent(tickerId: number) {
+  const { tickerDetail, isLoadingTickerDetail } = useTickerDetail(tickerId);
+
+  // Refactor: Save ETF aggregate detail
+  const [etfAggregateDetail, setETFAggregateDetail] = useState<
+    RustServiceETFAggregateDetail | undefined
+  >(undefined);
+  useEffect(() => {
+    if (tickerDetail?.is_etf) {
+      fetchETFAggregateDetailByTickerId(tickerDetail.ticker_id).then(
+        setETFAggregateDetail,
+      );
+    }
+  }, [tickerDetail]);
+
+  // Initial layout definition
+  const initialValue: MosaicNode<string> = useMemo(
+    () => ({
+      direction: "column",
+      first: {
+        direction: "row",
+        first: "Ticker Information",
+        second: {
+          direction: "row",
+          first: "Historical Prices",
+          second: {
+            direction: "row",
+            first: "Similarity Search",
+            second: "Sector Allocation",
+            splitPercentage: 50,
+          },
+          splitPercentage: 55.6,
+        },
+        splitPercentage: 25,
+      },
+      second: {
+        direction: "row",
+        first: "ETF Holders",
+        second: "ETF Holdings",
+        splitPercentage: 50,
+      },
+      splitPercentage: 30,
+    }),
+    [],
+  );
+
+  // Map content to window titles
+  const contentMap = useMemo(
+    () => ({
+      "Ticker Information":
+        isLoadingTickerDetail || !tickerDetail ? (
+          <Center>Loading</Center>
+        ) : (
+          <TickerInformation tickerDetail={tickerDetail} />
+        ),
+      "ETF Holders":
+        isLoadingTickerDetail || !tickerDetail ? (
+          <Center>Loading</Center>
+        ) : (
+          <Scrollable>
+            <ETFHolderList tickerDetail={tickerDetail} />
+          </Scrollable>
+        ),
+      "ETF Holdings":
+        isLoadingTickerDetail || !tickerDetail ? (
+          <Center>Loading</Center>
+        ) : (
+          <Scrollable>
+            <ETFHoldingList etfTickerDetail={tickerDetail} />
+          </Scrollable>
+        ),
+      "Historical Prices":
+        isLoadingTickerDetail || !tickerDetail ? (
+          <Center>Loading</Center>
+        ) : (
+          <AutoScaler>
+            <HistoricalPriceChart
+              tickerSymbol={tickerDetail?.symbol}
+              formattedSymbolWithExchange={formatSymbolWithExchange(
+                tickerDetail,
+              )}
+            />
+          </AutoScaler>
+        ),
+      "Similarity Search":
+        isLoadingTickerDetail || !tickerDetail ? (
+          <Center>Loading</Center>
+        ) : (
+          <AutoScaler>
+            <PCAScatterPlot tickerDetail={tickerDetail} />
+          </AutoScaler>
+        ),
+      "Sector Allocation":
+        isLoadingTickerDetail || !etfAggregateDetail ? (
+          <Center>Loading</Center>
+        ) : (
+          etfAggregateDetail?.major_sector_distribution && (
+            <SectorsPieChart
+              majorSectorDistribution={
+                etfAggregateDetail.major_sector_distribution
+              }
+            />
+          )
+        ),
+    }),
+    [etfAggregateDetail, isLoadingTickerDetail, tickerDetail],
+  );
+
+  return {
+    initialValue,
+    contentMap,
+    tickerDetail,
+  };
+}
