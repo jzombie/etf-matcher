7fa19c6d9c4ea564d97793c8c50db4a33090f805
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-07 17:22:59 -0600

    Prototype buttons for `SimilaritySeach` applet

--- src/components/TickerViewWindowManager/applets/TickerSimilaritySearch.applet/TickerSimilaritySearch.applet.tsx
+++ src/components/TickerViewWindowManager/applets/TickerSimilaritySearch.applet/TickerSimilaritySearch.applet.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
+
+import Layout, { Content, Header } from "@layoutKit/Layout";
+
 import PCAScatterPlot from "@components/PCAScatterPlot";
 
 import { RustServiceTickerDetail } from "@utils/callRustService";
@@ -11,5 +15,32 @@ export type TickerSimilaritySearchAppletProps = {
 export default function TickerSimilaritySearchApplet({
   tickerDetail,
 }: TickerSimilaritySearchAppletProps) {
-  return <PCAScatterPlot tickerDetail={tickerDetail} />;
+  return (
+    <Layout>
+      <Header>
+        <Box sx={{ textAlign: "center", marginBottom: 2 }}>
+          <ToggleButtonGroup
+            // value={displayMode}
+            exclusive
+            // onChange={handleDisplayModeChange}
+            aria-label="Similarity search toggle"
+            size="small"
+          >
+            <ToggleButton value="radial" aria-label="Radial chart">
+              Radial
+            </ToggleButton>
+            <ToggleButton value="euclidean" aria-label="Euclidean">
+              Euclidean
+            </ToggleButton>
+            <ToggleButton value="cosine" aria-label="Cosine">
+              Cosine
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
+      </Header>
+      <Content>
+        <PCAScatterPlot tickerDetail={tickerDetail} />;
+      </Content>
+    </Layout>
+  );
 }
