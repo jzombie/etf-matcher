cc878139e1eb33b0028904946af14938db358b7b
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-09 12:01:00 -0600

    Rename `detail` to `financialDetail`

--- src/components/TickerViewWindowManager/applets/TickerFundamentals.applet/FinancialChartsGrid.tsx
+++ src/components/TickerViewWindowManager/applets/TickerFundamentals.applet/FinancialChartsGrid.tsx
@@ -30,12 +30,12 @@ function isETFAggregateDetail(
 export default function FinancialChartsGrid({
   tickerDetail,
 }: FinancialChartsGridProps) {
-  const { isLoading, detail } = useTicker10KDetail(
+  const { isLoading, detail: financialDetail } = useTicker10KDetail(
     tickerDetail.ticker_id,
     tickerDetail.is_etf,
   );
 
-  if (isLoading || !detail) {
+  if (isLoading || !financialDetail) {
     return <div>Loading...</div>;
   }
 
@@ -88,161 +88,161 @@ export default function FinancialChartsGrid({
             title="Revenue"
             chartData={createChartData(
               "Revenue",
-              isETFAggregateDetail(detail)
-                ? detail.avg_revenue_current
-                : detail.revenue_current,
-              isETFAggregateDetail(detail)
-                ? detail.avg_revenue_1_yr
-                : detail.revenue_1_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_revenue_2_yr
-                : detail.revenue_2_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_revenue_3_yr
-                : detail.revenue_3_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_revenue_4_yr
-                : detail.revenue_4_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_revenue_current
+                : financialDetail.revenue_current,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_revenue_1_yr
+                : financialDetail.revenue_1_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_revenue_2_yr
+                : financialDetail.revenue_2_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_revenue_3_yr
+                : financialDetail.revenue_3_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_revenue_4_yr
+                : financialDetail.revenue_4_yr,
             )}
-            detail={detail}
+            financialDetail={financialDetail}
             colorIndex={0}
           />
           <RenderChart
             title="Gross Profit"
             chartData={createChartData(
               "Gross Profit",
-              isETFAggregateDetail(detail)
-                ? detail.avg_gross_profit_current
-                : detail.gross_profit_current,
-              isETFAggregateDetail(detail)
-                ? detail.avg_gross_profit_1_yr
-                : detail.gross_profit_1_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_gross_profit_2_yr
-                : detail.gross_profit_2_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_gross_profit_3_yr
-                : detail.gross_profit_3_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_gross_profit_4_yr
-                : detail.gross_profit_4_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_gross_profit_current
+                : financialDetail.gross_profit_current,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_gross_profit_1_yr
+                : financialDetail.gross_profit_1_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_gross_profit_2_yr
+                : financialDetail.gross_profit_2_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_gross_profit_3_yr
+                : financialDetail.gross_profit_3_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_gross_profit_4_yr
+                : financialDetail.gross_profit_4_yr,
             )}
-            detail={detail}
+            financialDetail={financialDetail}
             colorIndex={1}
           />
           <RenderChart
             title="Operating Income"
             chartData={createChartData(
               "Operating Income",
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_income_current
-                : detail.operating_income_current,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_income_1_yr
-                : detail.operating_income_1_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_income_2_yr
-                : detail.operating_income_2_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_income_3_yr
-                : detail.operating_income_3_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_income_4_yr
-                : detail.operating_income_4_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_income_current
+                : financialDetail.operating_income_current,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_income_1_yr
+                : financialDetail.operating_income_1_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_income_2_yr
+                : financialDetail.operating_income_2_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_income_3_yr
+                : financialDetail.operating_income_3_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_income_4_yr
+                : financialDetail.operating_income_4_yr,
             )}
-            detail={detail}
+            financialDetail={financialDetail}
             colorIndex={2}
           />
           <RenderChart
             title="Net Income"
             chartData={createChartData(
               "Net Income",
-              isETFAggregateDetail(detail)
-                ? detail.avg_net_income_current
-                : detail.net_income_current,
-              isETFAggregateDetail(detail)
-                ? detail.avg_net_income_1_yr
-                : detail.net_income_1_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_net_income_2_yr
-                : detail.net_income_2_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_net_income_3_yr
-                : detail.net_income_3_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_net_income_4_yr
-                : detail.net_income_4_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_net_income_current
+                : financialDetail.net_income_current,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_net_income_1_yr
+                : financialDetail.net_income_1_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_net_income_2_yr
+                : financialDetail.net_income_2_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_net_income_3_yr
+                : financialDetail.net_income_3_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_net_income_4_yr
+                : financialDetail.net_income_4_yr,
             )}
-            detail={detail}
+            financialDetail={financialDetail}
             colorIndex={3}
           />
           <RenderChart
             title="Total Assets"
             chartData={createChartData(
               "Total Assets",
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_assets_current
-                : detail.total_assets_current,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_assets_1_yr
-                : detail.total_assets_1_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_assets_2_yr
-                : detail.total_assets_2_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_assets_3_yr
-                : detail.total_assets_3_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_assets_4_yr
-                : detail.total_assets_4_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_assets_current
+                : financialDetail.total_assets_current,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_assets_1_yr
+                : financialDetail.total_assets_1_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_assets_2_yr
+                : financialDetail.total_assets_2_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_assets_3_yr
+                : financialDetail.total_assets_3_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_assets_4_yr
+                : financialDetail.total_assets_4_yr,
             )}
-            detail={detail}
+            financialDetail={financialDetail}
             colorIndex={4}
           />
           <RenderChart
             title="Total Liabilities"
             chartData={createChartData(
               "Total Liabilities",
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_liabilities_current
-                : detail.total_liabilities_current,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_liabilities_1_yr
-                : detail.total_liabilities_1_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_liabilities_2_yr
-                : detail.total_liabilities_2_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_liabilities_3_yr
-                : detail.total_liabilities_3_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_total_liabilities_4_yr
-                : detail.total_liabilities_4_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_liabilities_current
+                : financialDetail.total_liabilities_current,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_liabilities_1_yr
+                : financialDetail.total_liabilities_1_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_liabilities_2_yr
+                : financialDetail.total_liabilities_2_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_liabilities_3_yr
+                : financialDetail.total_liabilities_3_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_total_liabilities_4_yr
+                : financialDetail.total_liabilities_4_yr,
             )}
-            detail={detail}
+            financialDetail={financialDetail}
             colorIndex={5}
           />
           <RenderChart
             title="Operating Cash Flow"
             chartData={createChartData(
               "Operating Cash Flow",
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_cash_flow_current
-                : detail.operating_cash_flow_current,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_cash_flow_1_yr
-                : detail.operating_cash_flow_1_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_cash_flow_2_yr
-                : detail.operating_cash_flow_2_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_cash_flow_3_yr
-                : detail.operating_cash_flow_3_yr,
-              isETFAggregateDetail(detail)
-                ? detail.avg_operating_cash_flow_4_yr
-                : detail.operating_cash_flow_4_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_cash_flow_current
+                : financialDetail.operating_cash_flow_current,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_cash_flow_1_yr
+                : financialDetail.operating_cash_flow_1_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_cash_flow_2_yr
+                : financialDetail.operating_cash_flow_2_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_cash_flow_3_yr
+                : financialDetail.operating_cash_flow_3_yr,
+              isETFAggregateDetail(financialDetail)
+                ? financialDetail.avg_operating_cash_flow_4_yr
+                : financialDetail.operating_cash_flow_4_yr,
             )}
-            detail={detail}
+            financialDetail={financialDetail}
             colorIndex={6}
           />
         </Box>
