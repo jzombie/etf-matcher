9cd6e1c98176244bc13c1bd3a0328fbbb679d134
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-14 20:24:21 -0600

    Add inset, dashed border

--- src/components/BucketImportFileDropModal.tsx
+++ src/components/BucketImportFileDropModal.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Center from "@layoutKit/Center";
+import Full from "@layoutKit/Full";
+
 import TransparentModal, { TransparentModalProps } from "./TransparentModal";
 
 export type BucketImportFileDropModalProps = Omit<
@@ -7,8 +10,26 @@ export type BucketImportFileDropModalProps = Omit<
   "children"
 >;
 
+const BORDER_INSET: number = 16;
+
 export default function BucketImportFileDropModal({
   ...rest
 }: BucketImportFileDropModalProps) {
-  return <TransparentModal {...rest}>[File Drop]</TransparentModal>;
+  return (
+    <TransparentModal {...rest}>
+      <Full style={{ padding: BORDER_INSET }}>
+        <Full
+          style={{
+            border: "8px dashed #cccccc",
+            textAlign: "center",
+          }}
+        >
+          {
+            // TODO: Finish implementing
+          }
+          <Center>[File Drop]</Center>
+        </Full>
+      </Full>
+    </TransparentModal>
+  );
 }
