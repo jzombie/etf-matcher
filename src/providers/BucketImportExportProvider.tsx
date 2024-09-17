50aca3e75d4662599e950d2887c541f91b2bc192
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-17 10:05:00 -0600

    Prototype metadata parsing

--- src/providers/BucketImportExportProvider.tsx
+++ src/providers/BucketImportExportProvider.tsx
@@ -26,23 +26,38 @@ export default function BucketImportExportProvider({
   const [isImportExportModalOpen, setImportExportModalOpen] = useState(false);
   const [isDragOver, setIsDragOver] = useState(false);
 
-  // Function to open modal
   const openImportExportModal = useCallback(() => {
     setImportExportModalOpen(true);
   }, []);
 
-  // Function to close modal
   const closeImportExportModal = useCallback(() => {
     setImportExportModalOpen(false);
+  }, []);
+
+  const handleDrop = useCallback((evt: DragEvent) => {
+    evt.preventDefault(); // Prevent default browser behavior (e.g., opening the file)
+
+    const { files } = evt.dataTransfer as DataTransfer;
+
+    // TODO: Unify file handling to work consistently with drag and manual file selection
+    if (files && files.length > 0) {
+      // Iterate through the files and log their metadata
+      Array.from(files).forEach((file) => {
+        customLogger.debug("-----");
+        customLogger.debug("File Name:", file.name);
+        customLogger.debug("File Size:", file.size);
+        customLogger.debug("File Type:", file.type);
+        customLogger.debug("Last Modified:", file.lastModified);
+      });
+    } else {
+      customLogger.warn("No files detected in the drop event");
+    }
   }, []);
 
   return (
     <FileDragDropProvider
       onDragOverStateChange={setIsDragOver}
-      onDrop={(evt) => {
-        // TODO: Implement
-        customLogger.debug("drop", evt);
-      }}
+      onDrop={handleDrop}
     >
       <BucketImportExportContext.Provider
         value={{ openImportExportModal, closeImportExportModal }}
