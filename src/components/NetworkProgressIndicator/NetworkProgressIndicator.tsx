5cf9c10b1925af1e10b2025ac54cfca1ecd7aeeb
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-09-22 12:17:01 -0600

    Add fade-in/out effect to `building cache` indicator

--- src/components/NetworkProgressIndicator/NetworkProgressIndicator.tsx
+++ src/components/NetworkProgressIndicator/NetworkProgressIndicator.tsx
@@ -4,6 +4,8 @@ import { Box, CircularProgress, Typography } from "@mui/material";
 
 import NetworkRequestIndicator from "@components/MainLayout/Footer/NetworkRequestIndicator";
 
+import styles from "./NetworkProgressIndicator.module.scss";
+
 export default function NetworkProgressIndicator() {
   return (
     <Box>
@@ -12,7 +14,11 @@ export default function NetworkProgressIndicator() {
       </Box>
       <NetworkRequestIndicator style={{ opacity: 0.5 }} />
       <Box sx={{ textAlign: "center" }}>
-        <Typography variant="body2" color="secondary">
+        <Typography
+          variant="body2"
+          color="secondary"
+          className={styles.fadeInOut}
+        >
           Building cache...
         </Typography>
       </Box>
