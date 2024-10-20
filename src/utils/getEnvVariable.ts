bddc02c2eb87aed55103f1202387f783fb3c3b9a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-10-20 11:28:07 -0600

    Add comments

--- src/utils/getEnvVariable.ts
+++ src/utils/getEnvVariable.ts
@@ -1,3 +1,9 @@
+/**
+ * Retrieves the vale of an environment variable.
+ *
+ * This wrapper is used primarily to have a consistent way to handle errors if
+ * the variable is not found.
+ */
 export default function getEnvVariable<T = string>(key: string): T {
   const value = import.meta.env[key];
   if (value === undefined) {
