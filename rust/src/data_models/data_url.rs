28215fad2049a297d92533b2b3e9c71d00973c1c
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-10-24 08:24:11 -0600

    Use `rev-2` (extended training) variant of `r5`

--- rust/src/data_models/data_url.rs
+++ rust/src/data_models/data_url.rs
@@ -28,13 +28,16 @@ impl DataURL {
             DataURL::TickerETFHoldersShardIndex => "/data/ticker_etf_holders_shard_index.enc",
             DataURL::ETFAggregateDetailShardIndex => "/data/etf_aggregate_detail_shard_index.enc",
             DataURL::ETFHoldingTickersShardIndex => "/data/etf_holding_tickers_shard_index.enc",
+            //
+            // 10-K
             // DataURL::FinancialVectors10K => "/data/financial_vectors.tenk.bin", // TODO: Use encoded bin, provided that compression doesn't actually increase the file size
             // DataURL::FinancialVectors10K => "/data/r2.financial_vectors.tenk.bin", // TODO: Use encoded bin, provided that compression doesn't actually increase the file size
             // DataURL::FinancialVectors10K => "/data/r4.financial_vectors.tenk.bin", // TODO: Use encoded bin, provided that compression doesn't actually increase the file size
             // DataURL::FinancialVectors10K => "/data/r4a.financial_vectors.tenq.bin", // TODO: Use encoded bin, provided that compression doesn't actually increase the file size
             //
             // 10-Q
-            DataURL::FinancialVectors10K => "/data/r5-5-10-20yr.financial_vectors.tenq.bin", // TODO: Use encoded bin, provided that compression doesn't actually increase the file size
+            // DataURL::FinancialVectors10K => "/data/r5-5-10-20yr.financial_vectors.tenq.bin", // TODO: Use encoded bin, provided that compression doesn't actually increase the file size
+            DataURL::FinancialVectors10K => "/data/r5-5-10-20yr(rev-2).financial_vectors.tenq.bin", // TODO: Use encoded bin, provided that compression doesn't actually increase the file size
             //
             // DataURL::FinancialVectors10K => "/data/key_metrics.tenk.bin", // Prototype
             DataURL::Image(_) => panic!("Use image_url() for image paths"), // Prevent calling value() for images
