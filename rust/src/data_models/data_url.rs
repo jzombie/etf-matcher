a87546f9df3cba608a1bc493f09633a48cc12267
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-02 09:35:52 -0600

    Improve data URL generation

--- rust/src/data_models/data_url.rs
+++ rust/src/data_models/data_url.rs
@@ -1,4 +1,5 @@
 use crate::utils::ticker_vector_config_utils::get_ticker_vector_config_by_key;
+use std::path::PathBuf;
 
 pub enum DataURL {
     DataBuildInfo,
@@ -16,48 +17,50 @@ pub enum DataURL {
     Image(String),
 }
 
+// In relation to the web root
+const DATA_BASE_PATH: &str = "/data";
+
 impl DataURL {
     pub fn value(&self) -> String {
         match self {
-            DataURL::DataBuildInfo => "/data/data_build_info.enc".to_string(),
-            // DataURL::TickerByIdIndex => "/data/ticker_by_id_index.enc".to_string(),
-            DataURL::ExchangeByIdIndex => "/data/exchange_by_id_index.enc".to_string(),
-            DataURL::SectorByIdIndex => "/data/sector_by_id_index.enc".to_string(),
-            DataURL::IndustryByIdIndex => "/data/industry_by_id_index.enc".to_string(),
-            DataURL::TickerSearch => "/data/ticker_search_dict.enc".to_string(),
-            DataURL::TickerDetailShardIndex => "/data/ticker_detail_shard_index.enc".to_string(),
+            DataURL::DataBuildInfo => Self::build_path("data_build_info.enc"),
+            // DataURL::TickerByIdIndex => Self::build_path("ticker_by_id_index.enc"),
+            DataURL::ExchangeByIdIndex => Self::build_path("exchange_by_id_index.enc"),
+            DataURL::SectorByIdIndex => Self::build_path("sector_by_id_index.enc"),
+            DataURL::IndustryByIdIndex => Self::build_path("industry_by_id_index.enc"),
+            DataURL::TickerSearch => Self::build_path("ticker_search_dict.enc"),
+            DataURL::TickerDetailShardIndex => Self::build_path("ticker_detail_shard_index.enc"),
             DataURL::Ticker10KDetailShardIndex => {
-                "/data/ticker_10k_detail_shard_index.enc".to_string()
+                Self::build_path("ticker_10k_detail_shard_index.enc")
             }
             DataURL::TickerETFHoldersShardIndex => {
-                "/data/ticker_etf_holders_shard_index.enc".to_string()
+                Self::build_path("ticker_etf_holders_shard_index.enc")
             }
             DataURL::ETFAggregateDetailShardIndex => {
-                "/data/etf_aggregate_detail_shard_index.enc".to_string()
+                Self::build_path("etf_aggregate_detail_shard_index.enc")
             }
             DataURL::ETFHoldingTickersShardIndex => {
-                "/data/etf_holding_tickers_shard_index.enc".to_string()
+                Self::build_path("etf_holding_tickers_shard_index.enc")
             }
             DataURL::TickerVectors(key) => {
-                // Use the generated HashMap to dynamically retrieve the TickerVector
-                let ticker_vector_config = get_ticker_vector_config_by_key(key)
-                    .expect(&format!("Key not found in ticker vectors map: {}", key));
-
-                // Concatenate the static prefix with the path
-                const PREFIX: &str = "/data/";
-
-                let url = format!("{}{}", PREFIX, ticker_vector_config.path);
-
-                url
+                match get_ticker_vector_config_by_key(key) {
+                    Some(config) => Self::build_path(&config.path),
+                    None => panic!("Key not found in ticker vectors map: {}", key), // Or return Result instead
+                }
             }
             DataURL::Image(_) => panic!("Use image_url() for image paths"), // Prevent calling value() for images
         }
     }
 
+    fn build_path(file_name: &str) -> String {
+        let full_path: PathBuf = [DATA_BASE_PATH, file_name].iter().collect();
+        full_path.to_string_lossy().into_owned()
+    }
+
     // Function to get full URL for images
     pub fn image_url(&self) -> String {
         match self {
-            DataURL::Image(filename) => format!("/data/images/{}", filename),
+            DataURL::Image(filename) => Self::build_path(&format!("images/{}", filename)),
             _ => panic!("Not an image URL"),
         }
     }
