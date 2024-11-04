3dfaccf6b83fa0caec5774161d5f57f3398969d9
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-04 08:28:51 -0600

    Add comment

--- rust/src/config.rs
+++ rust/src/config.rs
@@ -8,7 +8,7 @@ pub struct TickerVectorConfig {
     pub sort_order: u32,
     pub path: &'static str,
     pub description: Option<&'static str>,
-    pub last_training_time: &'static str,
+    pub last_training_time: &'static str, // Note: Ideally, this should be a `DateTime`
     pub vector_dimensions: u32,
     pub training_sequence_length: u32,
     pub training_data_sources: Vec<&'static str>,
