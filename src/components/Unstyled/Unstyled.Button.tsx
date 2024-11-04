661dff56ef044665a72d8044f1c92d5f67810e8d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-04 08:32:38 -0600

    Prevent unintended form submissions

--- src/components/Unstyled/Unstyled.Button.tsx
+++ src/components/Unstyled/Unstyled.Button.tsx
@@ -4,6 +4,10 @@ import Unstyled, { UnstyledProps } from "./Unstyled";
 
 export type UnstyledButtonProps = UnstyledProps<"button">;
 
-export default function UnstyledButton({ ...rest }: UnstyledButtonProps) {
-  return <Unstyled {...rest} as="button" />;
+export default function UnstyledButton({
+  type = "button", // Default type is "button" to prevent unintended form submissions
+  role = "button", // Role is set to "button" for accessibility purposes
+  ...rest
+}: UnstyledButtonProps) {
+  return <Unstyled {...rest} type={type} role={role} as="button" />;
 }
