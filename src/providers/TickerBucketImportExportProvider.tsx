50e5b48b59d87db59e5be078f51f9ebbc8c74bcb
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-05 09:38:37 -0600

    Update comment

--- src/providers/TickerBucketImportExportProvider.tsx
+++ src/providers/TickerBucketImportExportProvider.tsx
@@ -80,8 +80,8 @@ export default function TickerBucketImportExportProvider({
   }, [tickerBucketImportExportService]);
 
   // Note: These errors may contain validation errors from the Rust service
-  // and are currently echoing up verbatim to the UI, which isn't typical
-  // of other implementations of this.
+  // and are currently echoing up verbatim to the UI, which isn't a typical
+  // pattern for error handling in this app.
   const handleVerbatimImportError = useCallback(
     (err: Error | string | unknown) => {
       customLogger.error(err);
