76b7f3a738f5bbdfe971ee835a2d062351243d10
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-20 11:31:39 -0600

    Prevent static rendering of bucket manager in footer on smaller viewports

--- src/components/TickerViewWindowManager/TickerViewWindowManager.tsx
+++ src/components/TickerViewWindowManager/TickerViewWindowManager.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+
+import Layout, { Content, Footer } from "@layoutKit/Layout";
 
 import TickerContainer from "@components/TickerContainer";
 import WindowManager from "@components/WindowManager";
+
+import useWindowSize from "@hooks/useWindowSize";
 
 import TickerViewWindowManagerBucketManager from "./components/TickerViewWindowManager.BucketManager";
 import useTickerViewWindowManagerContent from "./hooks/useTickerViewWindowManagerContent";
@@ -18,16 +22,44 @@ export default function TickerViewWindowManager({
   const { initialLayout, contentMap, tickerDetail } =
     useTickerViewWindowManagerContent(tickerId, isTiling);
 
+  const { height: windowHeight } = useWindowSize();
+
+  // FIXME: Ideally, this should go into a context provider so that child
+  // components can know whether or not to render their own `TickerViewWindowManagerBucketManager`
+  // without having to attach new listeners to the DOM. As it currently
+  // stands, this component will be rendered twice on certain mobile viewports.
+  const shouldShowFooter = useMemo(() => {
+    if (!tickerDetail) {
+      return false;
+    }
+
+    if (isTiling) {
+      return true;
+    }
+
+    return windowHeight > 960;
+  }, [tickerDetail, isTiling, windowHeight]);
+
   return (
     <TickerContainer tickerId={tickerId}>
-      <WindowManager
-        onTilingStateChange={setIsTiling}
-        initialLayout={initialLayout}
-        contentMap={contentMap}
-      />
-      {tickerDetail && (
-        <TickerViewWindowManagerBucketManager tickerDetail={tickerDetail} />
-      )}
+      <Layout>
+        <Content>
+          <WindowManager
+            onTilingStateChange={setIsTiling}
+            initialLayout={initialLayout}
+            contentMap={contentMap}
+          />
+        </Content>
+        {shouldShowFooter && tickerDetail && (
+          <Footer>
+            {
+              <TickerViewWindowManagerBucketManager
+                tickerDetail={tickerDetail}
+              />
+            }
+          </Footer>
+        )}
+      </Layout>
     </TickerContainer>
   );
 }
