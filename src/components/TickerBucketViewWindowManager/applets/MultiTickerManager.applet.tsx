46df240a102106155d250f54879ef201889ecf3a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-20 12:42:59 -0600

    Prototype initial `LogarithmicSlider` integration

--- src/components/TickerBucketViewWindowManager/applets/MultiTickerManager.applet.tsx
+++ src/components/TickerBucketViewWindowManager/applets/MultiTickerManager.applet.tsx
@@ -7,10 +7,11 @@ import Checkbox from "@mui/material/Checkbox";
 import Scrollable from "@layoutKit/Scrollable";
 
 import AvatarLogo from "@components/AvatarLogo";
+// import LogarithmicSliderInputCombo from "@components/LogarithmicSliderInputCombo";
+import LogarithmicSlider from "@components/LogarithmicSlider";
 
 import useTickerSymbolNavigation from "@hooks/useTickerSymbolNavigation";
 
-// New import for quantity input
 import TickerBucketViewWindowManagerAppletWrap, {
   TickerBucketViewWindowManagerAppletWrapProps,
 } from "../components/TickerBucketViewWindowManager.AppletWrap";
@@ -30,7 +31,42 @@ export default function MultiTickerManagerApplet({
 
   const navigateToSymbol = useTickerSymbolNavigation();
 
-  // TODO: Add ability to copy the selected ticker symbols (search for `copySymbolsToClipboard` and refactor)
+  // TODO: Extract to a component
+  const renderQuantitySlider = (tickerId: number) => {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          paddingTop: 1,
+          paddingLeft: 4,
+          width: "100%",
+        }}
+        onClick={(e) => {
+          e.stopPropagation(); // Prevent triggering parent click handler
+        }}
+      >
+        <LogarithmicSlider
+          // aria-label="Quantity Slider"
+          defaultValue={1}
+          // valueLabelDisplay="auto"
+          min={0.001}
+          max={10000000} // Adjust range as necessary
+          step={1}
+          sx={{
+            color: "primary.main",
+            marginLeft: 2,
+            width: "80%",
+          }}
+          onChange={(evt, val) => {
+            evt.stopPropagation();
+
+            console.log(val);
+          }}
+        />
+      </Box>
+    );
+  };
 
   return (
     <TickerBucketViewWindowManagerAppletWrap
@@ -47,7 +83,8 @@ export default function MultiTickerManagerApplet({
             <Box
               key={tickerDetail.ticker_id}
               display="flex"
-              alignItems="center"
+              flexDirection="column" // Updated to stack slider vertically
+              alignItems="start"
               padding={1}
               sx={{
                 cursor: "pointer",
@@ -63,83 +100,83 @@ export default function MultiTickerManagerApplet({
                     })
               }
             >
-              <Checkbox
-                checked={isSelected}
-                onChange={(evt) => {
-                  evt.stopPropagation(); // Prevent the parent click handler from firing
-                  evt.target.checked
-                    ? selectTicker({
-                        tickerId: tickerDetail.ticker_id,
-                        exchangeShortName: tickerDetail.exchange_short_name,
-                        symbol: tickerDetail.symbol,
-                        quantity: 1,
-                      })
-                    : deselectTicker(tickerDetail.ticker_id);
-                }}
-              />
-              <AvatarLogo
-                tickerDetail={tickerDetail}
-                style={{ marginRight: 8 }}
-                onClick={(e) => e.stopPropagation()} // Prevent double triggering
-              />
-              <Box>
-                <Box display="flex" alignItems="center">
-                  {/* Symbol and Button */}
-                  <Box display="flex" alignItems="center" marginRight={1}>
+              <Box display="flex" alignItems="center" width="100%">
+                <Checkbox
+                  checked={isSelected}
+                  onChange={(evt) => {
+                    evt.stopPropagation(); // Prevent the parent click handler from firing
+                    evt.target.checked
+                      ? selectTicker({
+                          tickerId: tickerDetail.ticker_id,
+                          exchangeShortName: tickerDetail.exchange_short_name,
+                          symbol: tickerDetail.symbol,
+                          quantity: 1,
+                        })
+                      : deselectTicker(tickerDetail.ticker_id);
+                  }}
+                />
+                <AvatarLogo
+                  tickerDetail={tickerDetail}
+                  style={{ marginRight: 8 }}
+                  onClick={(e) => e.stopPropagation()} // Prevent double triggering
+                />
+                <Box>
+                  <Box display="flex" alignItems="center">
+                    <Box display="flex" alignItems="center" marginRight={1}>
+                      <Box
+                        sx={{
+                          fontWeight: "bold",
+                          marginRight: 1,
+                        }}
+                      >
+                        {tickerDetail.symbol}
+                      </Box>
+                      <Box fontSize="small" color="text.secondary">
+                        {tickerDetail.exchange_short_name}
+                      </Box>
+                    </Box>
+
                     <Box
                       sx={{
-                        fontWeight: "bold",
-                        marginRight: 1,
+                        display: "flex",
+                        alignItems: "center",
+                        cursor: "pointer",
+                      }}
+                      title="View Details"
+                      onClick={(e) => {
+                        e.stopPropagation(); // Prevent triggering parent click handler
+                        navigateToSymbol(tickerDetail.symbol);
                       }}
                     >
-                      {tickerDetail.symbol}
-                    </Box>
-                    <Box fontSize="small" color="text.secondary">
-                      {tickerDetail.exchange_short_name}
+                      <LinkIcon
+                        fontSize="small"
+                        sx={{
+                          marginLeft: 0.5,
+                          color: "text.secondary",
+                          "&:hover": { color: "primary.main" },
+                        }}
+                      />
                     </Box>
                   </Box>
 
-                  {/* Adjacent Link */}
                   <Box
                     sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      cursor: "pointer",
+                      fontSize: "small",
+                      color: "text.secondary",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      whiteSpace: "nowrap",
+                      maxWidth: "200px", // Adjust based on layout needs
+                      display: "inline-block",
+                      verticalAlign: "middle",
                     }}
-                    title="View Details"
-                    onClick={(e) => {
-                      e.stopPropagation(); // Prevent triggering parent click handler
-                      navigateToSymbol(tickerDetail.symbol);
-                    }}
+                    title={tickerDetail.company_name} // Tooltip for full name
                   >
-                    <LinkIcon
-                      fontSize="small"
-                      sx={{
-                        marginLeft: 0.5,
-                        color: "text.secondary",
-                        "&:hover": { color: "primary.main" },
-                      }}
-                    />
+                    {tickerDetail.company_name}
                   </Box>
                 </Box>
-
-                {/* Company Name */}
-                <Box
-                  sx={{
-                    fontSize: "small",
-                    color: "text.secondary",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    whiteSpace: "nowrap",
-                    maxWidth: "200px", // Adjust based on layout needs
-                    display: "inline-block",
-                    verticalAlign: "middle",
-                  }}
-                  title={tickerDetail.company_name} // Tooltip for full name
-                >
-                  {tickerDetail.company_name}
-                </Box>
               </Box>
+              {renderQuantitySlider(tickerDetail.ticker_id)}
             </Box>
           );
         })}
