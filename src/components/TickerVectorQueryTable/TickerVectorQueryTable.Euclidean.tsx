3d98ff283e60941300297de9d0afaf43a8f59a9c
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-27 11:17:02 -0600

    Improve loading state rendering

--- src/components/TickerVectorQueryTable/TickerVectorQueryTable.Euclidean.tsx
+++ src/components/TickerVectorQueryTable/TickerVectorQueryTable.Euclidean.tsx
@@ -8,14 +8,16 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Typography,
 } from "@mui/material";
 
 import Center from "@layoutKit/Center";
+import Cover from "@layoutKit/Cover";
+import Full from "@layoutKit/Full";
 import type { RustServiceTickerDetail } from "@services/RustService";
 
 import AvatarLogo from "@components/AvatarLogo";
 import NetworkProgressIndicator from "@components/NetworkProgressIndicator";
+import NoInformationAvailableAlert from "@components/NoInformationAvailableAlert";
 
 import useTickerSymbolNavigation from "@hooks/useTickerSymbolNavigation";
 import useTickerVectorQuery, {
@@ -63,83 +65,98 @@ export default function TickerVectorQueryTableEuclidean({
     }
   }, [tickerVectorConfigKey, fetchEuclidean, query]);
 
-  if (isLoading) {
-    return (
-      <Center>
-        <NetworkProgressIndicator />
-      </Center>
-    );
-  }
-
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Symbol</TableCell>
-            <TableCell>Company Name</TableCell>
-            <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-              Industry
-            </TableCell>
-            <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-              Sector
-            </TableCell>
-            <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-              ETF Expense Ratio
-            </TableCell>
-            <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-              Held in ETF
-            </TableCell>
-            <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
-              {
-                // TODO: Prefix with proper type
-              }
-              Euclidean Distance
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tickerDetails && tickerDetails.length > 0 ? (
-            tickerDetails.map((detail) => (
-              <TableRow
-                key={detail.ticker_id}
-                onClick={() => handleRowClick(detail.symbol)}
-                sx={{ cursor: "pointer" }}
-              >
-                <TableCell sx={{ textAlign: "center", fontWeight: "bold" }}>
-                  <AvatarLogo
-                    tickerDetail={detail as RustServiceTickerDetail}
-                  />
-                  <br />
-                  {detail.symbol}
-                </TableCell>
-                <TableCell>{detail.company_name}</TableCell>
+    <Full>
+      <TableContainer component={Paper}>
+        {(tickerDetails || !isLoading) && (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Symbol</TableCell>
+                <TableCell>Company Name</TableCell>
                 <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-                  {detail.industry_name || "N/A"}
+                  Industry
                 </TableCell>
                 <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-                  {detail.sector_name || "N/A"}
+                  Sector
                 </TableCell>
                 <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-                  <FormattedETFExpenseRatio tickerDetail={detail} />
+                  ETF Expense Ratio
                 </TableCell>
                 <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>
-                  {detail.is_held_in_etf ? "Yes" : "No"}
+                  Held in ETF
                 </TableCell>
                 <TableCell sx={{ display: { xs: "none", md: "table-cell" } }}>
-                  {detail.distance.toFixed(2)}{" "}
+                  {
+                    // TODO: Prefix with proper type
+                  }
+                  Euclidean Distance
                 </TableCell>
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={7}>
-                <Typography>No details available</Typography>
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            </TableHead>
+            <TableBody>
+              {tickerDetails && tickerDetails.length > 0 ? (
+                tickerDetails.map((detail) => (
+                  <TableRow
+                    key={detail.ticker_id}
+                    onClick={() => handleRowClick(detail.symbol)}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <TableCell sx={{ textAlign: "center", fontWeight: "bold" }}>
+                      <AvatarLogo
+                        tickerDetail={detail as RustServiceTickerDetail}
+                      />
+                      <br />
+                      {detail.symbol}
+                    </TableCell>
+                    <TableCell>{detail.company_name}</TableCell>
+                    <TableCell
+                      sx={{ display: { xs: "none", sm: "table-cell" } }}
+                    >
+                      {detail.industry_name || "N/A"}
+                    </TableCell>
+                    <TableCell
+                      sx={{ display: { xs: "none", sm: "table-cell" } }}
+                    >
+                      {detail.sector_name || "N/A"}
+                    </TableCell>
+                    <TableCell
+                      sx={{ display: { xs: "none", sm: "table-cell" } }}
+                    >
+                      <FormattedETFExpenseRatio tickerDetail={detail} />
+                    </TableCell>
+                    <TableCell
+                      sx={{ display: { xs: "none", sm: "table-cell" } }}
+                    >
+                      {detail.is_held_in_etf ? "Yes" : "No"}
+                    </TableCell>
+                    <TableCell
+                      sx={{ display: { xs: "none", md: "table-cell" } }}
+                    >
+                      {detail.distance.toFixed(2)}{" "}
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={7}>
+                    <NoInformationAvailableAlert>
+                      No details available
+                    </NoInformationAvailableAlert>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        )}
+      </TableContainer>
+      <Cover clickThrough>
+        {isLoading && (
+          <Center>
+            <NetworkProgressIndicator />
+          </Center>
+        )}
+      </Cover>
+    </Full>
   );
 }
