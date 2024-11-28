5a566bc531feede985a303082394e2799e4c8927
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-11-28 13:41:47 -0600

    Remove non-utilized import

--- src/components/TickerBucketViewWindowManager/applets/MultiTickerSectorAllocation.applet.tsx
+++ src/components/TickerBucketViewWindowManager/applets/MultiTickerSectorAllocation.applet.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 
-import { Box } from "@mui/material";
-
 import AutoScaler from "@layoutKit/AutoScaler";
 import {
   RustServiceTickerWeightedSectorDistribution,
