c0d3af3c3bdfc293dbabbae32ed98456f686f285
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-21 10:56:57 -0600

    Fix error where `ticker_symbol` was not found in JSON

--- rust/src/data_models/ticker_etf_holder.rs
+++ rust/src/data_models/ticker_etf_holder.rs
@@ -7,9 +7,16 @@ use crate::{DataURL, PaginatedResults};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+struct TickerETFHolderRaw {
+    // TODO: Rename to `etf_ticker_id`
+    ticker_id: TickerId,
+    etf_ticker_ids_json: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct TickerETFHolder {
     // TODO: Rename to `etf_ticker_id`
-    pub ticker_id: TickerId,
+    pub ticker_id: TickerId, // TODO: Remove?
     // TODO: Rename to `etf_ticker_symbol`
     pub ticker_symbol: TickerSymbol,
     pub etf_ticker_ids_json: String,
@@ -57,9 +64,6 @@ impl TickerETFHolder {
         page: usize,
         page_size: usize,
     ) -> Result<PaginatedResults<TickerSymbol>, JsValue> {
-        // Log the start of the function
-        // web_sys::console::debug_1(&format!("Fetching ticker ETF holders for ticker_id: {}", ticker_id).into());
-
         let url: &str = &DataURL::TickerETFHoldersShardIndex.value();
 
         let ticker_id = get_ticker_id(ticker_symbol.clone())
@@ -67,31 +71,17 @@ impl TickerETFHolder {
             .map_err(|_| JsValue::from_str("Could not locate ticker ID"))?;
 
         // Query shard for the ticker_id
-        // web_sys::console::debug_1(&format!("Querying shard for ticker_id: {}", ticker_id).into());
-        let holder = query_shard_for_id(url, &ticker_id, |detail: &TickerETFHolder| {
+        let holder = query_shard_for_id(url, &ticker_id, |detail: &TickerETFHolderRaw| {
             Some(&detail.ticker_id)
         })
         .await?
-        .ok_or_else(|| {
-            // web_sys::console::debug_1(&format!("Ticker not found in shard for ticker_id: {}", ticker_id).into());
-            JsValue::from_str(&format!("Ticker {} not found", ticker_id))
-        })?;
-
-        // web_sys::console::debug_1(&format!("Found holder for ticker_id: {}", ticker_id).into());
+        .ok_or_else(|| JsValue::from_str(&format!("Ticker {} not found", ticker_id)))?;
 
         // Parse the ETF ticker IDs JSON
-        // web_sys::console::debug_1(&format!("Parsing ETF ticker IDs JSON for ticker_id: {}", ticker_id).into());
         let etf_ticker_ids: Vec<TickerId> = serde_json::from_str(&holder.etf_ticker_ids_json)
             .map_err(|e| {
-                // web_sys::console::debug_1(&format!("Failed to parse etf_ticker_ids_json for ticker_id: {}: {}", ticker_id, e).into());
                 JsValue::from_str(&format!("Failed to parse etf_ticker_ids_json: {}", e))
             })?;
-
-        // TODO: Remove
-        // let etf_ticker_symbols = etf_ticker_ids
-        //     .iter()
-        //     .map(|ticker_id| get_ticker_symbol(*ticker_id))
-        //     .collect();
 
         let etf_ticker_symbols = futures::future::join_all(
             etf_ticker_ids
@@ -104,10 +94,8 @@ impl TickerETFHolder {
         .collect();
 
         // Paginate the results
-        // web_sys::console::debug_1(&format!("Paginating results for ticker_id: {}", ticker_id).into());
         let paginated_results = PaginatedResults::paginate(etf_ticker_symbols, page, page_size)?;
 
-        // web_sys::console::debug_1(&format!("Returning paginated results for ticker_id: {}", ticker_id).into());
         Ok(paginated_results)
     }
 }
